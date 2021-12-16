95df450650dbc5ded1bc11113556c9ea688a9bbd
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2021-12-16 19:15:32 +0300

    add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        arr = []
+        for i in range(n+1):
+            if i == 0:
+                continue
+            if (i % 5) == 0 and (i % 3) == 0:
+                arr.append("FizzBuzz")
+            elif (i % 3) == 0:
+                arr.append("Fizz")
+            elif (i % 5) == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
+        return arr
