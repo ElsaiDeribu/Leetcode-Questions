556cdbbaa164b3de6322529d0084cce76ad50327
Yared <yaredtsegaye120@gmail.com>
2021-12-24 00:21:02 +0300

    A2SV Questions

---
+++ sortColors.py
@@ -0,0 +1,6 @@
+def sortColors(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
