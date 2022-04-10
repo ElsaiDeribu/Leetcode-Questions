a30020c67c398906ae3b1a7d2707a9a0afc98ee2
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-04-10 15:49:22 +0300

    Time: 155 ms (53.31%), Space: 18.1 MB (87.64%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, intervals):
+        ans =[]
+        intervals.sort()
+        ans.append(intervals[0])
+        for i in range (len(intervals)):
+            if ans[-1][-1] >= intervals[i][0]:
+                    if ans[-1][-1] <= intervals[i][1]:
+                         ans[-1][-1] = intervals[i][1]
+                    else:
+                        continue
+            else:  
+                ans.append(intervals[i])
+        return ans              
