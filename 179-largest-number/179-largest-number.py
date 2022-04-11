5ad2dc01eaeaf1373c334c229f33272d1eb51deb
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-04-11 10:19:26 +0300

    Time: 162 ms (5.01%), Space: 13.8 MB (69.15%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,13 +1,12 @@
-class Solution(object):
-    def largestNumber(self,  nums):
-        flag = 0
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+
         for k in range(len(nums)):
             if nums[k] != 0:
-                flag = 1;
-        
-        if flag == 0:
-            return "0"
-            
+                break
+            if k == len(nums)-1:
+                return "0"
+
         for i in range(len(nums)):
             for j in range(i, len(nums)):
                 comb_1 = int(str(nums[i]) + str(nums[j]))
