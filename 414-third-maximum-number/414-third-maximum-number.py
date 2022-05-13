e7e5bb3ea9b40975c440a56ab02f6087c5fb46fb
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-14 00:07:27 +0300

    Time: 50 ms (95.24%), Space: 15.6 MB (15.52%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.py
+++ 414-third-maximum-number/414-third-maximum-number.py
@@ -1,13 +1,48 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         
-        dic = Counter(nums)
+        temp = set(nums)
+        nums = list(temp)
+        
+        if len(nums) < 3:
+            return max(nums)
+        
+        print(nums)
+     
+        
+        lst = []
+        
+        for i in range(len(nums)):
+            lst.sort()
+            if len(lst) < 3:
+                lst.append(nums[i])      
+            
+            else:
+                
+                if nums[i] >= lst[-1]:
+                    lst.pop(0)
+                    lst.append(nums[i])
+                elif nums[i] >= lst[-2]:
+                    lst.pop(0)
+                    lst.insert(1, nums[i])
+                elif nums[i] >= lst[-3]:
+                    lst[-3] = nums[i]
+        lst.sort()
+            
+        return lst[0]
+            
+        
+        
+        
+       ## O(nlogn) time complexity solution
+    
+#         dic = Counter(nums)
 
-        sorted_dic = sorted(dic.items(), key=lambda x: x[0])
+#         sorted_dic = sorted(dic.items(), key=lambda x: x[0])
         
-        if len(sorted_dic) < 3:
-            return max(dic)
+#         if len(sorted_dic) < 3:
+#             return max(dic)
         
-        else:
-            return sorted_dic[-3][0]
-        print(sorted_dic)
+#         else:
+#             return sorted_dic[-3][0]
+#         print(sorted_dic)
