36a77ca28f833e6dba9faf73c821b9005068d329
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-15 12:51:15 +0300

    Time: 84 ms (73.99%), Space: 14 MB (66.44%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,25 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        dir = [(0,1),(1,0),(0,-1),(-1,0)]
+        
+        visited = set()
+        prevColor = image[sr][sc]
+        image[sr][sc] = newColor
+        row = len(image)
+        col = len(image[0])
+        
+        def dfs(row, col, prevColor, newColor, visited, image, r, c):
+            visited.add((r,c))
+            for i in dir:
+                coord = (r + i[0], c + i[1])
+                if 0 <= coord[0] < row and 0 <= coord[1] < col:
+                    if coord not in visited and image[coord[0]][coord[1]] == prevColor :
+                        if image[coord[0]][coord[1]] == prevColor:
+                            image[coord[0]][coord[1]] = newColor
+                        dfs(row, col, prevColor, newColor, visited, image, coord[0], coord[1])
+                    
+                
+            
+        dfs(row, col, prevColor, newColor, visited, image, sr, sc)
+        
+        return image
