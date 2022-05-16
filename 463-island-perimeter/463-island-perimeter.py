504bcc909e512c7403a09edcf9b00e500eb40c7b
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-16 15:58:18 +0300

    Time: 780 ms (38.79%), Space: 23.1 MB (14.85%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -14,19 +14,19 @@ class Solution:
         self.perimeter = 0
          
         def dfs(r, c):
+            
                 visited.add((r, c))
                 for i in direction:
                     coord = (r + i[0], c + i[1])
                     if (coord[0],coord[1]) not in visited:
                         if 0 <= coord[0] < self.row and 0 <= coord[1] < self.col and grid[coord[0]][coord[1]] == 1:
                             dfs(coord[0], coord[1])
-
                         elif self.row <= coord[0] or coord[0] < 0 or self.col <= coord[1] or coord[1]  < 0:
                             self.perimeter += 1
-                            
                         elif grid[coord[0]][coord[1]] == 0:
                             self.perimeter += 1
                         
                 
         dfs(r, c)
+        
         return self.perimeter
