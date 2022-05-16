bdd307b4207051c66a2add85ac2120fac4b0ef31
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-16 04:00:13 +0300

    Time: 80 ms (49.94%), Space: 16.3 MB (81.40%) - LeetHub

--- 563-binary-tree-tilt/563-binary-tree-tilt.py
+++ 563-binary-tree-tilt/563-binary-tree-tilt.py
@@ -26,19 +26,3 @@ class Solution:
         
         
         
-        
-#                 if not root:
-#             return 0
-#         count= 0
-#         def dfs(count, n):
-#             count += 1
-
-#             ans = 0
-#             for i in n.children:
-#                 ans = max(ans, dfs(count, i))
-           
-#             return count if not n.children else ans
-         
-#         return dfs(count, root)
-        
-        
