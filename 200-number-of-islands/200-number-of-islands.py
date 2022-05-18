608139d5cff0aabfbb0cc9b842dc80394d2486b8
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-18 12:28:09 +0300

    Time: 371 ms (64.21%), Space: 25.2 MB (8.66%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set()
+        ans = 0
+        
+        def dfs(n):
+            visited.add(n)
+            for i in directions:
+                coord = (n[0] + i[0], n[1] + i[1])
+                if 0<= coord[0] < len(grid) and 0 <= coord[1] < len(grid[0]) and coord not in visited and grid[coord[0]][coord[1]] == '1':
+                    dfs(coord)
+             
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    dfs((i,j))
+                    ans += 1
+                    
+        return ans
+
+         
