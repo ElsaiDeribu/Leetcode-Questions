4fb034dba4b74c917d83660f7135d233410d929d
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-18 13:48:45 +0300

    Time: 530 ms (21.97%), Space: 25.2 MB (9.45%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -8,7 +8,7 @@ class Solution:
             visited.add(n)
             for i in directions:
                 coord = (n[0] + i[0], n[1] + i[1])
-                if 0<= coord[0] < len(grid) and 0 <= coord[1] < len(grid[0]) and coord not in visited and grid[coord[0]][coord[1]] == '1':
+                if 0 <= coord[0] < len(grid) and 0 <= coord[1] < len(grid[0]) and coord not in visited and grid[coord[0]][coord[1]] == '1':
                     dfs(coord)
              
         for i in range(len(grid)):
