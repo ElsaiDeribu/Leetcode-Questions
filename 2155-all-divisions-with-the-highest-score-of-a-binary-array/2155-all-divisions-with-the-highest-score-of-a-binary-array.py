b5d0d1aa72fc235447c2614b991d9792bc449fdb
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-05-24 12:06:51 +0300

    Time: 6792 ms (5.10%), Space: 47.2 MB (7.14%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -4,9 +4,10 @@ class Solution:
         postfix = [0]*(len(nums)+1)
         postfix[-1] = 0
         scores = defaultdict(list)
-        
         maximum = 0
         counter = 0
+        
+        
         for i in range(len(nums) ):
             if nums[i] == 0:
                 counter += 1
@@ -20,15 +21,13 @@ class Solution:
              
             postfix[i] = counter
             
-        # print(prefix)
-        # print(postfix)
         
         for i in range(len(nums) + 1):
             
             scores[(prefix[i] + postfix[i])].append(i)
-            
             maximum = max(maximum, (prefix[i] + postfix[i]) )
-            
+          
+        
         return scores[maximum]
             
         
