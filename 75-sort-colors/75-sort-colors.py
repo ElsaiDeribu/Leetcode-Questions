129dde2616beb7eece678ba367b57ed3107a1cd4
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-07-22 04:22:10 -0700

    Time: 46 ms (65.71%), Space: 13.7 MB (97.26%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,11 +1,12 @@
 class Solution:
-    def sortColors(self, arr: List[int]) -> None:
+    def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        
         """
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                if arr[i] < arr[j]:
-                    arr[i], arr[j] = arr[j], arr[i]
-        return arr
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] > nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    
+        print(nums)
+        
