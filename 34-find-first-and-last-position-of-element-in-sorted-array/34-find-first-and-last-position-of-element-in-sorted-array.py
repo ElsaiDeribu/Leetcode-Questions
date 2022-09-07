49a9a1800593c7093ebfaaee6bdebce414395141
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-09-07 21:15:36 +0300

    Time: 164 ms (21.62%), Space: 15.4 MB (47.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,41 +1,61 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        
-        def findLeftIndex():
-            index = -1
-            left = 0
-            right = len(nums) - 1
-
+        def findLeft(nums):
             
-            while left <= right:
-                mid = left + (right - left)//2
-                if nums[mid] >= target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-                if nums[mid] == target:
-                    index = mid
-            return index 
+            start, end, index = 0, len(nums) - 1, -1
+            
+            while start <= end:
                 
-        def findRightIndex():
-            index = -1
-            left = 0
-            right = len(nums) - 1
-            
-
-            while left <= right:
-                mid = left + (right - left)//2
-                if nums[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+                mid = start + (end - start) // 2
+                
+                if nums[mid] >= target:
+                    
+                    end = mid - 1 
+                    
+                else: 
+                    start = mid + 1
+                    
                 if nums[mid] == target:
+                    
                     index = mid
+                
                     
             return index 
+        
+        
+        def findRight(nums):
+            
+            index = -1
+            start = 0
+            end = len(nums) - 1
+            
+            while start <= end:
                 
-        return (findLeftIndex(), findRightIndex())
+                mid = start + (end - start) // 2
+                
+                if nums[mid] <= target:
+                    
+                    start = mid + 1
+                    
+                else:
+                    end = mid - 1
+                    
+                if nums[mid] == target:
+                    
+                    index = mid
+                    
+            return index
+            
+        
+        print(findRight(nums))
+        return [findLeft(nums), findRight(nums)]
+                    
+                    
+            
+            
+        
+
         
         
         
