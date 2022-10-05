2218bd3a103751ef43ec047999cae46ffcbe9feb
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-10-05 13:37:26 +0300

    Time: 543 ms (41.00%), Space: 27.6 MB (10.67%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,76 +1,79 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         
-#         for i in range(1, len(nums)):
-            
-#             nums[i] = nums[i] + nums[i - 1]
+         
+         # prifix sum with sliding window solution
         
-#         i = 0
-#         j= 0
-#         minimum = 10**9 
-#         sumation = nums[0]
-#         print(nums)
-        
-#         while i < len(nums):
-            
-#             if minimum == 1 :
-#                 return minimum
-            
-#             while sumation < target and j < len(nums):
+            for i in range(1,len(nums)):
+                nums[i] = nums[i] + nums[i - 1]
                 
-#                 if i == 0:
-#                     sumation = nums[j]
-#                 else:
-#                     sumation = nums[j] - nums[i - 1]
-#                 if sumation < target:
-#                     j += 1
-                
-#             while sumation >= target and i <= j:  
-#                 if i == 0:
-#                     sumation = nums[j]
-#                 else:
-#                     sumation = nums[j] - nums[i - 1]
-#                 if sumation >= target:    
-#                     i += 1
-            
-#             if sumation >= target:    
-#                 length = (j - i + 1)
-#                 minimum = min(minimum, length)
-
-            
-            
-#         print(minimum)
-        
-    
-
-            i = 0
-            j = 0
+            nums.insert(0,0)
+            windowSum = nums[1]
             minLength = 10**9
-            windowSum = nums[i]
-
+            
+            i = j = 1
 
             while j < len(nums):
                 
                 if minLength == 1:
-                    return minLength
+                    return 1
                 
                 while j < len(nums) and windowSum < target:
+                    
                     j += 1
                     if j < len(nums):
-                        windowSum += nums[j]
-
+                        windowSum = nums[j] - nums[i - 1]
+                        
+                        
                 if windowSum >= target:
                     minLength = min(minLength, j - i + 1)
-
+                    
+                    
                 while i < j and windowSum >= target:
-                    windowSum -= nums[i]
                     i += 1
+                    windowSum = nums[j] - nums[i - 1]
+                    
                     if windowSum >= target:
                         minLength = min(minLength, j - i + 1)
-                        
+                
             if minLength == 10**9:
                 return 0
+            
             return minLength
+        
+        
+        
+        # sliding window solution
+    
+        
+#             i = 0
+#             j = 0
+#             minLength = 10**9
+#             windowSum = nums[i]
+
+
+#             while j < len(nums):
+                
+#                 if minLength == 1:
+#                     return minLength
+                
+#                 while j < len(nums) and windowSum < target:
+#                     j += 1
+#                     if j < len(nums):
+#                         windowSum += nums[j]
+
+#                 if windowSum >= target:
+#                     minLength = min(minLength, j - i + 1)
+
+#                 while i < j and windowSum >= target:
+#                     windowSum -= nums[i]
+#                     i += 1
+#                     if windowSum >= target:
+#                         minLength = min(minLength, j - i + 1)
+                        
+#             if minLength == 10**9:
+#                 return 0
+#             return minLength
 
             
             
