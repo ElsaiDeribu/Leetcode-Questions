dbd8415639dfe1c8000960cbd19fc4811f77a7d8
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-10-28 11:18:31 +0300

    Time: 4000 ms (12.13%), Space: 32.9 MB (30.93%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,33 +1,24 @@
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        changed.sort()
-        count = Counter(changed)
-        ans =[]
-        if len(changed) % 2 !=0:
+        
+        if len(changed) % 2:
             return []
-        for k in count: 
-            if k == 0:
-                if count[k] % 2 != 0:
-                    return []
-            if ((2 * k) not in count.keys()) and ((k/2) not in count.keys()):
-                return []
-
-        if (not(any(changed))):
-            changed =  changed[:(len(changed))//2]
-            return changed
-
-        for i in count:
-            if count[i] == 0:
-                    continue
-            if (i== 0):
-                count[i] = (count[i]//2)
-            elif (2 * i) in count.keys():
-                if count[2 * i ] >= count[i]:
-                    count[2 * i] = count[2 * i] - count[i]
-                else:
-                    return []
-
-        for i in count:
-            for j in range(count[i]):
-                ans.append(i)
-        return ans
+        
+        changed.sort()
+        ans = []
+        count = Counter(changed)
+        
+        
+        for i in range(len(changed) ):
+            
+            if changed[i] == 0 and count[0] >= 2:
+                ans.append(0)
+                count[0] -= 2
+            
+            if changed[i] > 0 and (count[changed[i]] > 0) and (count[2 * changed[i]] > 0 ):
+                ans.append(changed[i])
+                count[changed[i]] -= 1
+                count[2 * changed[i]] -= 1
+                
+        
+        return ans if len(ans) == len(changed) // 2 else []
