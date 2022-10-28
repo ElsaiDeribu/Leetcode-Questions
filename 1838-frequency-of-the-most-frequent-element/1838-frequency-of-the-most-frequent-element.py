a86156186d0b5534e649d3ed7420e99f80de00c3
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-10-28 19:51:44 +0300

    Time: 2902 ms (62.22%), Space: 28.5 MB (25.06%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,33 +1,24 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        numbers = sorted(nums)
-        i = 0
-        j = 1
-        freq= 0
-        if len(nums) == 1:
-            return 1
-        sum = numbers[i] + numbers[j]
-        while i < j:
-            if((j-i+1)*numbers[j])-sum <=k:
-                freq = max(freq, (j-i+1))
-                if j < len(numbers) - 1:
-                    j += 1
-                    sum += numbers[j]
-                    
-                else:
-                    break
-            elif ((j-i+1)*numbers[j])-sum > k:
-                    sum -= numbers[i]
-                    i += 1
-                                       
-                    if j < len(numbers) - 1 and i == j:
-                   
-                        j += 1
-                        sum += numbers[j]
-                       
-        if freq == 0:
-            return 1
         
-        return freq
-
         
+        nums = sorted(nums, reverse = True)  
+            
+        l = r = 0
+        subArrSum = 0
+        longest = 0
+        
+        while r < len(nums):
+            
+            subArrSum += nums[r]
+            
+            while nums[l] * (r - l + 1) - subArrSum > k:
+                subArrSum -= nums[l]
+                l += 1
+            
+            longest = max(longest, r - l + 1)
+            
+            r += 1
+        
+        
+        return longest
