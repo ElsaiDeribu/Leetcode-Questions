1674a1999cba66cbbf39408598824184a5e452cf
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-10-31 08:49:34 +0300

    Time: 700 ms (17.03%), Space: 14.6 MB (82.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,35 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         
-        tCount = Counter(t)
-        debit = len(tCount)
-        minLen = ""
-        mx = 10**6
+        debit = Counter(t)
+        count = len(debit)
+        mn = 10**6
+        ans = ""
         
         i = j = 0
         
         
         while j < len(s):
             
-            while j < len(s) and debit:
-                
-                if s[j] in tCount:
-                    tCount[s[j]] -= 1
-                    if tCount[s[j]] == 0:
-                        debit -= 1
-                
+            while j < len(s) and count:
+                if s[j] in debit:
+                    debit[s[j]] -= 1
+                    if debit[s[j]] == 0:
+                        count -= 1
+                        
                 j += 1
                 
-            while debit == 0 and i < j:
-                if j - i  < mx:
-                    mx = j - i 
-                    minLen = s[i:j]
-                   
-                if s[i] in tCount:
-                    tCount[s[i]] += 1
-                    if tCount[s[i]] > 0:
-                        debit += 1
-            
+            while i < j and count == 0:
+                if j - i < mn:
+                    mn = j - i
+                    ans = s[i:j]
+                    
+                if s[i] in debit:
+                    debit[s[i]] += 1
+                    if debit[s[i]] > 0:
+                        count += 1
+                        
                 i += 1
-            
-        return minLen
+          
+        
+        return ans
