59bfdeb319de36942eb4227c3418c125adf46aff
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-01 16:46:14 +0300

    Time: 47 ms (93.30%), Space: 13.9 MB (57.05%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -1,23 +1,32 @@
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
         
-        reqIndex = None
-        ans = [0] * len(s)
+        
+        closest = [0] * len(s)
+        
+        req = None
         
         for i in range(len(s)):
+            
             if s[i] == c:
-                reqIndex = i
-            elif reqIndex != None:
-                ans[i] = i - reqIndex
-            else:
-                ans[i] = 10**5
+                closest[i] = "c"
+                req = i
                 
-        reqIndex = None
-        
-        for i in range(len(s) - 1, -1, -1):
+            elif req != None:
+                closest[i] = i - req
+                
+        req = None        
+        for i in range(len(s) - 1, -1 , -1):
             if s[i] == c:
-                reqIndex = i
-            elif reqIndex:
-                ans[i] = min(ans[i], reqIndex - i)
+                closest[i] = 0
+                req = i
+                
+            elif req != None and closest[i] != 0:
+                
+                closest[i] = min(closest[i],req - i )
+            
+            elif req != None:
+                closest[i] = req - i
         
-        return ans
+            
+        return closest
