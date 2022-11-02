98c753d5ea2dff5c570b642b6151f3e6e09fa2dd
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-02 07:50:33 +0300

    Time: 38 ms (85.57%), Space: 13.9 MB (72.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,28 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
-        st = []
         
         dic = {'(':')', '{':'}', '[':']'}
-        
+        st = []
         
         for i in range(len(s)):
-            
             if s[i] in dic.keys():
                 st.append(s[i])
-            
+                
             else:
                 
                 if st and dic[st[-1]] == s[i]:
                     st.pop()
                     
-                    
                 else:
                     return False
                 
                 
-                
-        return False if st else True
-            
-                
-            
+        return True if not st else False
