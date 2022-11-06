7acfd90b5daa8f858398931c471d21f8af8528df
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-06 18:43:38 +0300

    Time: 197 ms (34.34%), Space: 14.1 MB (59.04%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        st = []
+        
+        
+        for i in range(len(s)):
+            
+            if s[i] == 'c' and len(st) >= 2:
+                b = st.pop()
+                a = st.pop()
+                
+                if b != "b" or a != "a":
+                    st.append(a)
+                    st.append(b)
+                    st.append(s[i])
+                
+            else:
+                st.append(s[i])
+          
+        return False if st else True
