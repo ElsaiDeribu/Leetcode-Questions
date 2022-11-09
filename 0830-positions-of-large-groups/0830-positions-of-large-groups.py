118aab4550f74b5db682a1b8cc1f04fbcc0c7912
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-09 08:47:42 +0300

    Time: 105 ms (5.63%), Space: 13.9 MB (74.57%) - LeetHub

--- 0830-positions-of-large-groups/0830-positions-of-large-groups.py
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.py
@@ -8,11 +8,11 @@ class Solution:
             while s[i] != s[j]:
                 i += 1
             j += 1
-            if j < len(s) and s[i] != s[j]:
+            if j == len(s) or  (j < len(s) and s[i] != s[j]):
                 if j - i >= 3:
                     ans.append([i, j - 1])
-            elif j == len(s):
-                if j - i >= 3:
-                    ans.append([i, j - 1])
+            # elif j == len(s):
+            #     if j - i >= 3:
+            #         ans.append([i, j - 1])
                     
         return ans
