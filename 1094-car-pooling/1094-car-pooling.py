3aa3652c722ee4779c04c25103b1b8947f327003
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-10 00:16:20 +0300

    Time: 179 ms (29.98%), Space: 14.4 MB (88.12%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,31 +1,25 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         
-        length = 0
+        size = 0
+        for i in range(len(trips)):
+            size = max(size, trips[i][2])
         
-        for num, f, t in trips:
+        prefixes = [0] * (size + 1)
+        
+        for i in range(len(trips)):
+            prefixes[trips[i][1]] += trips[i][0]
+            prefixes[trips[i][2]] -= trips[i][0]
+        
+        print(prefixes)
+        for i in range(1, len(prefixes)):
+            prefixes[i] = prefixes[i - 1] + prefixes[i]
             
-            length =  max(length, t)
-            
-        ans = [0] * (length + 1)
-        
-        for nums, s, e in trips:
-            ans[s] += nums
-            ans[e] -= nums
-        print(ans)
-        
-        if ans[0] > capacity:
-            return False
-        
-        for i in range(1, len(ans)):
-            ans[i] = ans[i] + ans[i - 1]
-            
-            if i < len(ans) - 1 and ans[i] > capacity:
+        for i in range(len(prefixes) - 1):    
+            if i < len(prefixes) and prefixes[i] > capacity:
                 return False
             
+        return True
+                
             
-        return True 
-            
-            
-            
-            
+        
