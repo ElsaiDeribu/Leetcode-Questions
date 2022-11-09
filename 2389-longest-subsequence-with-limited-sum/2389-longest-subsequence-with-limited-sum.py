ca8ce88c7f4cca446c934ecce1ab3240f503ac55
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-09 13:00:43 +0300

    Time: 993 ms (27.19%), Space: 14.2 MB (40.50%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        ans = []
+        nums.sort()
+        print(nums)
+        
+        for i in range(len(queries)):
+            l = 0
+            sumation = nums[l]
+            while sumation <= queries[i] and l < len(nums):
+                l += 1
+                if l < len(nums):
+                    sumation += nums[l]
+            
+            print(nums[:l])
+            print(sumation)
+            ans.append(len(nums[:l]))
+            
+        return ans
