54154ea9ec13c511d7c59f7f861fd828140c538a
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-09 09:55:58 +0300

    Time: 3341 ms (22.10%), Space: 25.7 MB (69.08%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -3,21 +3,17 @@ class Solution:
         
         l = 0
         r = k - 1
-        
         subArrSum = sum(nums[l: r + 1])
         subArrAverage = subArrSum / k
         maxAverage = subArrAverage
         
         while r < len(nums):
-            
             r += 1
             if r < len(nums):
                 subArrSum += nums[r]
                 subArrSum -= nums[l]
                 l += 1
-            
             subArrAverage = subArrSum / k
-            
             maxAverage = max(maxAverage, subArrAverage)
         
         return maxAverage   
