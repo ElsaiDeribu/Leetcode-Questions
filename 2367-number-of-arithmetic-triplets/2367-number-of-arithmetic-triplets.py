535c58639ae3d0a1097975025b6628b2cdf5ccee
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-10 13:03:39 +0300

    Time: 2270 ms (16.82%), Space: 13.9 MB (19.55%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -8,10 +8,7 @@ class Solution:
         for i in range(len(nums) - 2):
             for j in range(i + 1, len(nums) - 1):
                 for k in range(j + 1, len(nums)):
-                    
                     if nums[j] - nums[i] == nums[k] - nums[j] == diff:
-                        
-                        # visited.add((nums[i], nums[j], nums[k]))
                         count += 1
                         
         return count
