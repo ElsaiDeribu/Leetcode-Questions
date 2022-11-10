a523c0cd21b14af39495ecc42ab2bb8c9f6e4a74
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-11 00:10:59 +0300

    Time: 930 ms (81.00%), Space: 28.2 MB (22.04%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        prefSum = [0] * len(nums)
+        prefSum[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            prefSum[i] = prefSum[i - 1] + nums[i]
+        
+        # print(prefSum)
+        
+        largestSum = -10**6
+        minleftSum = 0
+        
+        for i in range(len(prefSum)):
+            largestSum = max(largestSum, prefSum[i] - minleftSum )
+            minleftSum = min(minleftSum, prefSum[i])
+            
+        return largestSum
