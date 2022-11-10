226cd1f1c1913a77122a91a29b591025fcf30000
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-10 10:50:13 +0300

    Time: 158 ms (74.19%), Space: 17.8 MB (33.96%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -6,13 +6,7 @@ class NumArray:
         for i in range(1, len(nums)):
             self.prefixes[i] = self.prefixes[i - 1] + self.prefixes[i]
         
-    def sumRange(self, left: int, right: int) -> int:
-#         total = 0
-#         if left == 0:
-#             total = self.prefixes[right]
-            
-#         else:
-#             total =  
+    def sumRange(self, left: int, right: int) -> int: 
             
         return self.prefixes[right] - self.prefixes[left - 1] if left else  self.prefixes[right]
 
