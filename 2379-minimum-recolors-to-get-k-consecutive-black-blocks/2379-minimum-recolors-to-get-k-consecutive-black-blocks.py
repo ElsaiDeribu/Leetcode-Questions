dd18c0f906e0230e5e3b3dfc7a0d6f90e9b606f0
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-12 16:26:39 +0300

    Time: 46 ms (79.44%), Space: 14 MB (21.61%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,23 +1,50 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         
-        count = defaultdict(int)
-        ans  = 1000
         
-        for i in range(k):
-            count[blocks[i]] += 1
+#         count = defaultdict(int)
+#         ans  = 1000
+        
+#         for i in range(k):
+#             count[blocks[i]] += 1
             
-        r = i
-        l = 0
+#         r = i
+#         l = 0
         
-        while r < len(blocks):
-            ans = min(ans, count['W'])
-            count[blocks[l]] -= 1
-            l += 1
-            r += 1
-            if r < len(blocks):
-                count[blocks[r]] += 1
+#         while r < len(blocks):
+#             ans = min(ans, count['W'])
+#             count[blocks[l]] -= 1
+#             l += 1
+#             r += 1
+#             if r < len(blocks):
+#                 count[blocks[r]] += 1
 
-        return ans
-            
+#         return ans
+
+
+            l = 0
+            windowCount = 0
+            minWinCount = 10**6
+
+            for i in range(k):
+                if blocks[i] == 'W':
+                    windowCount += 1
+            r = i
+
+            minWinCount = min(minWinCount, windowCount)
+
+            while r < len(blocks):
+                r += 1
+                if r < len(blocks) and blocks[r] == 'W':
+                    windowCount += 1
+
+                if blocks[l] == 'W':
+                    windowCount -= 1
+                l += 1
+
+                if r < len(blocks):
+                    minWinCount = min(minWinCount, windowCount)
+
+            return minWinCount
+
         
