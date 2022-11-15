d61f7b2f08b632a36ed7329ef05edbd483098404
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-15 09:38:32 +0300

    Time: 206 ms (92.14%), Space: 18.7 MB (39.88%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,16 +2,22 @@ class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
         heap = []
-        
-        
-        
-        
-        
-        
-        # Sorting solution
-        elements = []
-        for i in matrix:
-            elements.extend(i)
+        ans = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                heappush(heap, matrix[i][j])
                 
-        elements.sort()
-        return elements[k - 1]
+        while k:
+            ans = heappop(heap)
+            k-= 1
+        
+        return ans
+        
+        
+#         # Sorting solution
+#         elements = []
+#         for i in matrix:
+#             elements.extend(i)
+                
+#         elements.sort()
+#         return elements[k - 1]
