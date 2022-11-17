371d3c722387f5e3aebf46cd837cf453abaf3798
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-18 01:23:41 +0300

    Time: 288 ms (85.15%), Space: 27.1 MB (45.25%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,18 +1,71 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         
-        l = 0
-        r = 0
-        subarrSum = 0
-        minLength = 10 ** 6
+        l = r = 0
+        minimalLen = 10**6
+        subArrSum = nums[0]
         
         while r < len(nums):
-            subarrSum += nums[r]
-            while subarrSum >= target:
-                minLength = min(minLength, r - l + 1)
-                subarrSum -= nums[l]
+            while subArrSum >= target:
+                minimalLen = min(minimalLen, r - l + 1)
+                subArrSum -= nums[l]
                 l += 1
                 
             r += 1
+            if r < len(nums):
+                subArrSum += nums[r]
+            
+        return 0 if minimalLen == 10**6 else minimalLen
         
-        return 0 if minLength == 10**6 else minLength
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         l = 0
+#         r = 0
+#         subarrSum = 0
+#         minLength = 10 ** 6
+        
+#         while r < len(nums):
+#             subarrSum += nums[r]
+#             while subarrSum >= target:
+#                 minLength = min(minLength, r - l + 1)
+#                 subarrSum -= nums[l]
+#                 l += 1
+                
+#             r += 1
+        
+#         return 0 if minLength == 10**6 else minLength
