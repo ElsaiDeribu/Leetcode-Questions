7d688f3e05c86300c85d1d132d1719b7eaa56aad
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-17 21:53:25 +0300

    Time: 501 ms (32.22%), Space: 14.1 MB (57.35%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -1,22 +1,16 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        
         ans = []
-        arth = 0
+        def isArithmetic(arr):
+            d = arr[1] - arr[0]
+            for i in range(len(arr) - 1):
+                if arr[i + 1] - arr[i] != d:
+                    return False
+            return True
+        
         for i in range(len(l)):
-            subArr = nums[l[i]:(r[i] + 1)]
-            subArr.sort()
-            d =  subArr[1] - subArr[0] 
-            for j in range(len(subArr)-1):
-                if( (subArr[j+1] - subArr[j] ) != d):
-                    arth = 0
-                    break
-                else:
-                    arth = 1
-
-            if arth :
-                ans.append (True)
-            else: 
-                ans.append(False)
+            subarr = sorted(nums[l[i]: r[i] + 1])
+            ans.append(isArithmetic(subarr))
+            
         return ans
-                    
-                    
