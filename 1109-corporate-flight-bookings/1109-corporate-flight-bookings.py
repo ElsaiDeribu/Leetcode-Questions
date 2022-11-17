135e7fd0147260eab86956a284560ea82fec8655
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-17 23:21:04 +0300

    Time: 953 ms (81.95%), Space: 28.1 MB (69.24%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,16 +1,14 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
         
-        ans = [0]*(n + 1)
+        prefixes = [0] * (n + 1)
         
         
-        for start, end, seats in bookings:
-            ans[start - 1] += seats
-            ans[end] -= seats
-            
-            
-        for i in range(1,len(ans)):
-            ans[i] = ans[i - 1] + ans[i]
-            
-        return ans[:-1]
+        for i in range(len(bookings)):
+            prefixes[bookings[i][0] - 1] += bookings[i][2]
+            prefixes[bookings[i][1]] -= bookings[i][2]
         
+        for i in range(1, len(prefixes)):
+            prefixes[i] = prefixes[i - 1] + prefixes[i]
+        
+        return prefixes[:-1]
