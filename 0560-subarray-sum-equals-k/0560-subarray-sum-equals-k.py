683d20b5f3a7d36af9fa7895979ffb273ff49d8f
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-18 00:48:28 +0300

    Time: 813 ms (32.00%), Space: 18.3 MB (13.99%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -12,63 +12,6 @@ class Solution:
             prefixes[currSum] += 1
             
         return count
-            
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         prefixes = defaultdict(int)
-#         prefixes[0] = 1
-#         sumUpToNow = 0
-#         count = 0
-        
-#         for i in range(len(nums)):
-#             sumUpToNow += nums[i]
-#             if prefixes[sumUpToNow - k] > 0:
-#                 count += prefixes[sumUpToNow - k]
-                
-#             prefixes[sumUpToNow] += 1
-            
-#         return count
                 
             
