ffd69884e37d98502100e2b712be7d2f4b5b7727
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-18 09:47:37 +0300

    Time: 512 ms (29.25%), Space: 14.1 MB (34.02%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,45 +1,18 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         
-#         theInteger = []
-        
-#         factor2 = []
-#         factor3 = []
-#         factor5 = []
-        
-#         heappush(factor2, 2)
-#         heappush(factor3, 3)
-#         heappush(factor5, 5)
-#         heappush(theInteger, 1)
-        
-#         while len(theInteger) < n:
-            
-#             minOfThree = min(factor2[0], factor3[0], factor5[0])
-            
-#             heappush(theInteger, minOfThree)
-#             heappush(factor2, -2 * minOfThree)
-#             heappush(factor3, -3 * minOfThree)
-#             heappush(factor5, -5 * minOfThree)
-        
-#         print(factor2, factor3, factor5)
-        # print(n)
-        mul = [2,3,5]
-        vstd = set()
+        primes = [2, 3, 5]
         heap = []
-        heapq.heappush(heap,1)
+        visited = set()
+        heappush(heap, 1)
+        
         while n:
-            curr = heapq.heappop(heap)
-            for i in mul:
-                if curr * i not in vstd:
-                    vstd.add(curr*i)
-                    heapq.heappush(heap,curr*i)
+            curr = heappop(heap)
+            for i in primes:
+                if curr * i not in visited:
+                    visited.add(curr * i)
+                    heappush(heap, curr * i)
+                    
             n -= 1
+            
         return curr
-            
-            
-            
-            
-        
-        
-        
-  
