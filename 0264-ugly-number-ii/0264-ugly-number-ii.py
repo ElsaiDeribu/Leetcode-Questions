17e064ec1a105c3845d97cff8af1e3ca10a597f6
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-19 01:57:39 +0300

    Time: 173 ms (84.78%), Space: 14.2 MB (20.91%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         
-        primes = [2, 3, 5]
+        mul = [2,3,5]
+        vstd = set()
         heap = []
-        visited = set()
         heappush(heap, 1)
         
         while n:
             curr = heappop(heap)
-            for i in primes:
-                if curr * i not in visited:
-                    visited.add(curr * i)
+            for i in mul:
+                if curr * i not in vstd:
+                    vstd. add(curr * i)
                     heappush(heap, curr * i)
                     
             n -= 1
