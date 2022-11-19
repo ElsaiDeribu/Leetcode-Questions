aee510ee772f2c9602267d1bfd024b03321d0d9f
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-19 11:26:19 +0300

    Time: 507 ms (49.30%), Space: 14.6 MB (51.63%) - LeetHub

--- 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -1,25 +1,17 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
-        
-        for i in range(len(hours)):
-            if hours[i] > 8:
-                hours[i] = 1
-            else:
-                hours[i] = -1
-        
+
         prefixes = defaultdict(int)
-        prefixes[0] = -1
+        # prefixes[0] = -1
         sumUpToNow = 0
         longest = 0
         
         for i in range(len(hours)):
-            sumUpToNow += hours[i]
+            sumUpToNow += (1 if hours[i] > 8 else -1) 
             if sumUpToNow > 0:
                 longest = max(longest, i + 1)
             elif sumUpToNow - 1 in prefixes:
                 longest = max(longest, i - prefixes[sumUpToNow - 1])
-                # if i - prefixes[sumUpToNow - 1] >  longest:
-                #     longest = i - prefixes[sumUpToNow - 1]
             if sumUpToNow not in prefixes:
                 prefixes[sumUpToNow] = i
 
