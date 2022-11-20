df20523b0c9e4713bc765d3d0ed591b7b3f3a880
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-11-20 20:23:21 +0300

    Time: 911 ms (87.38%), Space: 19.3 MB (75.16%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        prefixes = defaultdict(int)
+        sumUpToNow = 0
+        longest = 0
+        
+        for i in range(len(nums)):
+            sumUpToNow += 1 if nums[i] else -1
+            if sumUpToNow == 0:
+                longest = max(longest, i + 1)
+            elif sumUpToNow in prefixes:
+                longest = max(longest, i - prefixes[sumUpToNow])
+            if sumUpToNow not in prefixes:
+                prefixes[sumUpToNow] = i
+                
+        return longest
+            
+            
