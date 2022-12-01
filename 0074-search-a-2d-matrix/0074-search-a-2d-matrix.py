7e7d8bee3e3f154e8e660f8ea877140aed87bdd8
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-02 00:25:34 +0300

    Time: 77 ms (68.43%), Space: 14.4 MB (89.83%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        
+        def binSearch(row):
+            l = -1
+            r = len(matrix[0])
+            while l + 1 < r:
+                mid = (l + r) // 2
+                if matrix[row][mid] >= target:
+                    r = mid
+                else:
+                    l = mid
+            return True if matrix[row][r] == target else False
+        
+        
+        
+        for i in range(len(matrix)):
+            if matrix[i][-1] >= target:
+                return binSearch(i)
+            
+            
+        
