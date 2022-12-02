5b58ce65ff8e3ecb417c12cb1b3e45d319011517
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-02 13:38:02 +0300

    Time: 360 ms (62.20%), Space: 20.5 MB (12.35%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,17 +1,42 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        r = len(matrix) - 1
-        c = 0
         
-        while 0 <= r < len(matrix) and 0 <= c < len(matrix[0]):
+        # O(m + n) solution
+        
+#         r = len(matrix) - 1
+#         c = 0
+        
+#         while 0 <= r < len(matrix) and 0 <= c < len(matrix[0]):
             
-            if matrix[r][c] > target:
-                r -= 1
-            elif matrix[r][c] < target:
-                c += 1
-            elif matrix[r][c] == target:
+#             if matrix[r][c] > target:
+#                 r -= 1
+#             elif matrix[r][c] < target:
+#                 c += 1
+#             elif matrix[r][c] == target:
+#                 return True
+            
+            
+        
+        
+        # O(m log(n) )
+        def binSearch(row):
+            l = -1
+            r = len(matrix[0])
+            while l + 1 < r:
+                mid = (l + r) // 2
+                if matrix[row][mid] >= target:
+                    r = mid
+                else:
+                    l = mid
+            return matrix[row][r] if r != len(matrix[0]) else ""
+            
+            
+        for i in range(len(matrix)):
+            if binSearch(i) == target:
                 return True
-            
-            
-        # print("hello world")
+                
+                
+                
+                
+                
