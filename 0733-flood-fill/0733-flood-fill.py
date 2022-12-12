4e581223323b1b220e61de2044fac104b0badcc2
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-12 10:27:00 +0300

    Time: 194 ms (18.75%), Space: 14.3 MB (15.58%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -8,17 +8,17 @@ class Solution:
         row = len(image)
         col = len(image[0])
         
-        def dfs(row, col, prevColor, r, c):
+        def dfs(prevColor, r, c):
             visited.add((r,c))
             for i in dir:
                 coord = (r + i[0], c + i[1])
                 if 0 <= coord[0] < row and 0 <= coord[1] < col:
                     if coord not in visited and image[coord[0]][coord[1]] == prevColor :
                         image[coord[0]][coord[1]] = color
-                        dfs(row, col, prevColor, coord[0], coord[1])
+                        dfs(prevColor, coord[0], coord[1])
                             
             
-        dfs(row, col, prevColor, sr, sc)
+        dfs(prevColor, sr, sc)
         
         return image
         
