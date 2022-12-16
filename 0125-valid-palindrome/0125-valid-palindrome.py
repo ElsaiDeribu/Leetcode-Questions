348cdeed0e6ce6ec0089fb703c8183b1b31f4785
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-16 23:57:19 +0300

    Time: 59 ms (80.58%), Space: 14.6 MB (51.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        s2 = ""
-        
-        for i in range(len(s)):
-            
-            if ord('a') <= ord(s[i].lower()) <= ord('z') or ( ord('0') <= ord(s[i]) <= ord('9')):
-                s2 += s[i].lower()
-                
-                
         l = 0
-        r = len(s2) - 1
+        r = len(s) - 1
         
         while l < r:
-            
-            if s2[l] != s2[r]:
-                return False
-            l += 1
-            r -= 1
-            
+            if not s[l].isnumeric() and not s[l].isalpha() :
+                l += 1
+            elif not s[r].isnumeric() and not s[r].isalpha():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+                
         return True
-
+        
         
         
         
