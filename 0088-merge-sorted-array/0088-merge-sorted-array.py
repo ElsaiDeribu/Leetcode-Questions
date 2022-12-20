5fffcea51c1248cd4b3f742752b9ebe8b1f86d5f
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-20 22:27:35 +0300

    Time: 65 ms (61.26%), Space: 14.1 MB (6.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,50 +4,20 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        l = m - 1
-        r = n - 1
-        k = len(nums1) - 1
+        i = m - 1
+        j = m + n - 1
+        k = n - 1
         
-        while r >= 0 or l >= 0:
-          
-            if r >= 0 and (l < 0 or nums1[l] < nums2[r]):
-                nums1[k] = nums2[r]
-                r -= 1
-                k -= 1
-            else:
-                nums1[k] = nums1[l]
-                l -= 1
-                k -= 1
-         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         j = 0
-#         for i in range(m, len(nums1)):
-#             nums1[i] = nums2[j]
-#             j += 1
-            
-#         nums1.sort()
-        
-        
-        
-#         ans = []
-#         i = 0
-#         j = 0
-#         while i < m or j < n:
-#             if i < m and nums1[i] < nums2[j]:
-#                 ans.append(nums1[i])
-#                 i += 1
-#             else:
-#                 ans.append(nums2[j])
-#                 j += 1
+        while i >= 0 or k >= 0:
+            if (i >= 0 and k < 0) or (i >= 0 and nums1[i] >= nums2[k]):
+                nums1[j] = nums1[i]
+                j -= 1
+                i -= 1
                 
-#         print(ans)
+            elif (i < 0 and k >= 0) or (k >= 0 and nums1[i] < nums2[k]):
+                nums1[j] = nums2[k]
+                j -= 1
+                k -= 1
+                
         
-        
+    
