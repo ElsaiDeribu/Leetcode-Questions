24d11e2c016be555817a103c90eec8230310292a
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2022-12-25 22:35:05 +0300

    Time: 393 ms (79.86%), Space: 23.2 MB (18.77%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,21 +7,66 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         
-        ans = []
+        tree1 = []
+        tree2 = []
         
-        def dfs(node):
+        def dfs1(node):
             if not node:
                 return 
             
-            dfs(node.left)
-            dfs(node.right)
-            ans.append(node.val)
+            dfs1(node.left)
+            tree1.append(node.val)
+            dfs1(node.right)
             
-        dfs(root1)
-        dfs(root2)
+        def dfs2(node):
+            if not node:
+                return 
+            
+            dfs2(node.left)
+            tree2.append(node.val)
+            dfs2(node.right)
         
-        return sorted(ans)
+        
+        dfs1(root1)
+        dfs2(root2)
+        
+        i = 0
+        j = 0
+        ans = []
+        
+        while len(ans) < len(tree1) + len(tree2):
+            while (i < len(tree1) and j >= len(tree2)) or (i < len(tree1) and tree1[i] <= tree2[j]):
+                ans.append(tree1[i])
+                i += 1
             
+            while (j < len(tree2) and i >= len(tree1)) or (j < len(tree2) and tree2[j] < tree1[i]):
+                ans.append(tree2[j])
+                j += 1
+            
+            
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
