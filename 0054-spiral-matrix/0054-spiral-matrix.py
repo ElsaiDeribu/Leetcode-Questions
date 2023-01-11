27cd86654b82abd3a071fe62074d6ff5f411db6e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-01-11 21:55:02 +0300

    Time: 38 ms (62.58%), Space: 13.8 MB (81.50%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,37 +1,53 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-
-        t , l = 0, 0
-        b = len(matrix) - 1
-        r = len(matrix[0]) - 1 
         
-        direction = 0 
+        top = left = 0
+        right = len(matrix[0]) - 1
+        bottom = len(matrix) - 1
+        
+        
+        direction = 'right'
         ans = []
         
-        while t <= b and l <= r:
-            if direction == 0:
-                for i in range(l, r + 1):
-                    ans.append(matrix[t][i])
-                t += 1
-                direction = 1
-
-            elif direction == 1:
-                for i in range(t, b + 1):
-                    ans.append(matrix[i][r])
-                r -= 1
-                direction = 2
+        
+        while left <= right and bottom >= top:
+            
+            if direction == 'right':
+                for i in range(left, right + 1):
+                    ans.append(matrix[top][i])
                     
-            elif direction == 2:
-                for i in range(r, l - 1, - 1):
-                    ans.append(matrix[b][i])
-                    
-                b -= 1    
-                direction = 3
+                top += 1
+                direction = 'down'
                 
-            elif direction == 3:
-                for i in range(b, t - 1, - 1):
-                    ans.append(matrix[i][l])
-                l += 1
-                direction = 0
+            elif direction == 'down':
+                for i in range(top, bottom + 1):
+                    ans.append(matrix[i][right])
                     
+                right -= 1
+                direction = 'left'
+                
+            elif direction == 'left':
+                for i in range(right, left - 1, -1):
+                    ans.append(matrix[bottom][i])
+                                
+                bottom -= 1
+                direction = 'up'
+                
+            elif direction == 'up':
+
+                for i in range(bottom, top - 1, -1):
+                    ans.append(matrix[i][left])
+                
+                left += 1
+                direction = 'right'
+                
+
         return ans
+                
+                
+                    
+                    
+        
+        
+        
+        
