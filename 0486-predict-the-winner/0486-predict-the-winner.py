c5aed87c32d864286becc5500787f0f3c4a11e0e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-03-02 13:41:26 +0300

    Time: 7267 ms (5.03%), Space: 13.8 MB (79.57%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,29 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        
+        
+        left, right = 0, len(nums) - 1
+        p1, p2  = 0, 0
+        turn = 1
+        
+        def helper(p1, p2, left, right, turn):
+            
+            if left > right:
+                return True if p1 >= p2 else False
+            
+            
+            if turn == 1:
+                
+                poss1 = helper(p1 + nums[left], p2, left + 1 , right, 2)
+                poss2 = helper(p1 + nums[right], p2, left, right - 1, 2)
+
+                return poss1 or poss2
+            
+            else:
+                
+                poss1 = helper(p1, p2 + nums[left], left + 1 , right, 1)
+                poss2 = helper(p1, p2 + nums[right], left, right - 1, 1)
+
+                return poss1 and poss2
+                
+        return helper(p1, p2, left, right, turn)
