911b70bcaff6da92ab9e8cf83771380e65fbdff5
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-03-07 05:32:27 +0300

    Time: 363 ms (45.52%), Space: 20.2 MB (54.28%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.py
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -4,25 +4,25 @@ class Solution:
         
         ans = [0] * len(intervals)
         
-        std = [(index, val[0]) for index, val in enumerate(intervals)]
-        std = sorted(std, key = lambda x : x[1] )
+        sortd = [(index, val[0]) for index, val in enumerate(intervals)]
+        sortd = sorted(sortd, key = lambda x : x[1] )
         
         def findRight(num):
             
             left = -1
-            right = len(std) - 1
+            right = len(sortd) - 1
             
             
             while left + 1 < right:
                 mid = left + (right - left)//2
                 
-                if std[mid][1] >= num:
+                if sortd[mid][1] >= num:
                     right = mid
                 
                 else:
                     left = mid
             
-            return std[right]
+            return sortd[right]
             
         for i in range(len(intervals)):
             
