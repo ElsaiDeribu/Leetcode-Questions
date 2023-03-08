6ec08b2beffe5aa303eda51c9a79af736c0b0e17
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-03-08 11:44:06 +0300

    Time: 46 ms (20.11%), Space: 13.8 MB (67.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -14,22 +14,52 @@ class Solution:
         curr2 = list2
         currM = merged
         
-        while curr1 and curr2:
+#         while curr1 and curr2:
             
-            if curr1.val >= curr2.val:
-                currM.next = ListNode(curr2.val)
-                currM = currM.next
-                curr2 = curr2.next
+#             if curr1.val >= curr2.val:
+#                 currM.next = ListNode(curr2.val)
+#                 currM = currM.next
+#                 curr2 = curr2.next
+                
+#             else:
+#                 currM.next = ListNode(curr1.val)
+#                 currM = currM.next
+#                 curr1 = curr1.next
+                
+#         currM.next = curr1 if curr1 else curr2
+        
+#         return merged.next
+    
+    
+        def recur(curr1, curr2, currM):
+            
+            if not curr1 and not curr2:
+                return 
+            
+            if not curr1 and curr2:
+                currM.next = curr2
+                
+                recur(curr1, curr2.next, currM.next)
+                
+            elif curr1 and not curr2:
+                currM.next = curr1
+                recur(curr1.next, curr2, currM.next)
                 
             else:
-                currM.next = ListNode(curr1.val)
-                currM = currM.next
-                curr1 = curr1.next
                 
-        currM.next = curr1 if curr1 else curr2
+                if curr1.val >= curr2.val:
+                    currM.next = curr2
+                    recur(curr1, curr2.next, currM.next)
+                    
+                else:
+                    currM.next = curr1
+                    recur(curr1.next, curr2, currM.next)
+          
+        recur(curr1, curr2, currM)
         
         return merged.next
-        
+                    
+                
 
 
 
