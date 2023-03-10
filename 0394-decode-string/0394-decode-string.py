0cc1a7e36c135c128e8be291d48cbaa941bc3e9f
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-03-10 15:03:04 +0300

    Time: 22 ms (98.27%), Space: 13.9 MB (59.10%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,40 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        
+        ans = []
+        
+        def recur(start):
+            
+            i = start
+            temp = ""
+            num = 0
+            
+            while i < len(s):
+                
+                if s[i].isnumeric():
+                    num = num * 10 + int(s[i])
+                  
+                elif s[i] == "[":
+                    result, currIndex = recur(i + 1)
+                    i = currIndex 
+                    
+                    temp += (result * num) if num != 0 else result
+                    num = 0
+                    
+                elif s[i] == "]":
+                    
+                    break
+                    
+                else:
+                    temp += s[i]
+                  
+                i += 1
+            
+            
+            return (temp, i)
+
+                
+        return recur(0)[0]
+        
+        
