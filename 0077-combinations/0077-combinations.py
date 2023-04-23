b799cdd493076844351fc7317c6538e11879c91e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-04-23 10:48:30 +0300

    Time: 401 ms (63.87%), Space: 15.9 MB (82.09%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,6 +1,8 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
+        
+        
         ans = []
         comb = []
         
@@ -10,24 +12,54 @@ class Solution:
                 ans.append(comb[:])
                 return
             
-#             if i > n:
-#                 return 
-            
-#             comb.append(i)
-#             recur(i + 1)
-#             comb.pop()
-#             recur( i + 1)
-            
             
             for i in range(start, n + 1):
                 
                 comb.append(i)
                 recur(i + 1)
                 comb.pop()
-                    
+        
+        
+        
         recur(1)
         
         return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ans = []
+#         comb = []
+        
+#         def recur(start):
+            
+#             if len(comb) == k:
+#                 ans.append(comb[:])
+#                 return
+            
+# #             if i > n:
+# #                 return 
+            
+# #             comb.append(i)
+# #             recur(i + 1)
+# #             comb.pop()
+# #             recur( i + 1)
+            
+            
+#             for i in range(start, n + 1):
+                
+#                 comb.append(i)
+#                 recur(i + 1)
+#                 comb.pop()
+                    
+#         recur(1)
+        
+#         return ans
                 
             
             
