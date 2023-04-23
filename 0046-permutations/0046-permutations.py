bc5936c0ac541e8e17fb083984c15db3ef3145ab
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-04-23 11:29:11 +0300

    Time: 47 ms (22.07%), Space: 14 MB (48.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,25 +1,86 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
+        
         def recur(arr):
             
             if len(arr) == 1:
-                return [arr.copy()] 
-            
+                return [arr[:]] 
+             
             result = []
-            
-            for _ in range(len(arr)):
-                temp = arr.pop(0)
-                perms = recur(arr)
-                arr.append(temp)
+            for i in range(len(arr)):
+                n = arr.pop(0)
+                nxt = recur(arr)
+                arr.append(n)
                 
-                for perm in perms:
-                    perm.append(temp)
+                for item in nxt:
+                    item.append(n)
+                    
+                    
+                result.extend(nxt)
                 
-                result.extend(perms)
-               
             return result
         
         
-        
         return recur(nums)
+                
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def recur(arr):
+            
+#             if len(arr) == 1:
+#                 return [arr.copy()] 
+            
+#             result = []
+            
+#             for _ in range(len(arr)):
+#                 temp = arr.pop(0)
+#                 perms = recur(arr)
+#                 arr.append(temp)
+                
+#                 for perm in perms:
+#                     perm.append(temp)
+                
+#                 result.extend(perms)
+               
+#             return result
+        
+        
+        
+#         return recur(nums)
