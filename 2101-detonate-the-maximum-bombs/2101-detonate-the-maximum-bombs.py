72e39b337a46308b69d3d49f01a5bfdb40c7230a
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-04-25 18:23:03 +0300

    Time: 784 ms (75.91%), Space: 14.5 MB (39.29%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        
+        adjList = [[] for i in range(len(bombs))]
+      
+        
+        for i in range(len(bombs)):
+            for j in range(len(bombs)):
+                if i != j:
+                    distance = sqrt((bombs[i][0] - bombs[j][0]) ** 2 + (bombs[i][1] - bombs[j][1]) ** 2)
+                    rad = bombs[i][2] 
+
+                    if distance <= rad:
+                        adjList[i].append(j)
+    
+        
+        maxBom = 1
+        exploded = set()
+        
+        print(adjList)
+    
+        def explode(index):
+            total = 1
+            for idx in adjList[index]:
+                if idx not in exploded:
+                    exploded.add(idx)
+                    total += explode(idx)
+                    
+            return total
+        
+        
+        
+        for i in range(len(adjList)):
+            exploded.add(i)
+            maxBom = max(maxBom, explode(i))
+            exploded = set()
+        
+
+        return maxBom
