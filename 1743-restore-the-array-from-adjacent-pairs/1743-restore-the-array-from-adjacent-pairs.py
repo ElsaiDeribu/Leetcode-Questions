89a0fd25147d638e0f084cb60f7c55bfd2f4ff41
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-05-15 16:26:35 +0300

    Time: 1301 ms (23.04%), Space: 168.4 MB (13.55%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.py
@@ -2,21 +2,18 @@ class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
         
         adjList = defaultdict(list)
-        edgeCnt = defaultdict(int)
         order = []
         
         for n1, n2 in adjacentPairs:
             adjList[n1].append(n2)
             adjList[n2].append(n1)
             
-            edgeCnt[n1] += 1
-            edgeCnt[n2] += 1
-            
+     
         
         def dfs(node, parent):
             nonlocal order
             
-            if edgeCnt[node] == 1:
+            if len(adjList[node]) == 1:
                 order.append(node)
                 return
 
@@ -28,13 +25,12 @@ class Solution:
             order.append(node)
 
         
-        for node in edgeCnt:
-            if edgeCnt[node] == 1:
+        for node in adjList:
+            if len(adjList[node]) == 1:
                 child = adjList[node][0]
                 dfs(child, node)
                     
                 order.append(node)
-                break
                 
-        return order
+                return order
        
