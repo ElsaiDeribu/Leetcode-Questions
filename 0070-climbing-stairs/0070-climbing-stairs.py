204da51b61f2b7d6dd6ce7b1ff1cd2048b440cab
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-05-26 18:45:27 +0300

    Time: 49 ms (16.28%), Space: 16.2 MB (28.83%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        
+        @cache
+        def recur(step):
+            
+            if step == 0:
+                return 1
+            
+            if step == -1:
+                return 0
+            
+            p1 = step - 1  
+            p2 = step - 2 
+            
+            res1 = recur(p1)
+            res2 = recur(p2)
+            
+            total =  res1 + res2 
+            
+            return total 
+        
+        
+        return recur(n)
