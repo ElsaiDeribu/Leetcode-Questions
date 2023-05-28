4064eeec94d66fdf97d8e8de4ab37089e07478d3
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-05-28 14:59:05 +0300

    Time: 46 ms (30.39%), Space: 16.3 MB (34.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,9 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         
+        memo = {}
         
-        @cache
         def trib(n):
+            
+            if n in memo:
+                return memo[n]
             
             if n == 2:
                 return 1
@@ -11,7 +14,9 @@ class Solution:
             if n <= 1:
                 return n
             
-            return trib(n - 1) + trib(n - 2) + trib(n - 3)
-        
-        
+            res =  trib(n - 1) + trib(n - 2) + trib(n - 3)
+            memo[n] = res
+            
+            return res
+            
         return trib(n)
