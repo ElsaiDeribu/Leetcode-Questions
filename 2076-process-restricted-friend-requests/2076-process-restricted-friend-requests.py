452834930fcd016fef9fce1a5399dc64c4373837
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-05-30 11:03:36 +0300

    Time: 3883 ms (59.17%), Space: 17.2 MB (37.50%) - LeetHub

---
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -0,0 +1,49 @@
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        
+        parent = {i:i for i in range(n)}
+        ans = [True] * len(requests)
+        
+        
+        def find(x):
+            nonlocal parent
+            
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+                
+            return x
+        
+        def union(x, y):
+            nonlocal parent
+            
+            repX = find(x)
+            repY = find(y)
+            
+            if repX == repY:
+                return 
+            
+            parent[repX] = repY
+            
+        
+        for i in range(len(requests)):
+            
+            n1 = requests[i][0]
+            p1 = find(n1)
+            n2 = requests[i][1]
+            p2 = find(n2)
+            
+            conn = {p1, p2}
+            
+            
+            for r1, r2 in restrictions:
+                if {find(r1), find(r2)} == conn :
+                    ans[i] = False
+                    break
+            else:
+                union(n1, n2)
+                
+
+        return ans
+        
+        
