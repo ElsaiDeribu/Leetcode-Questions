5f70014cd5616d2ceeee48ba35ef77683a867ef2
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-05-30 19:15:11 +0300

    Time: 79 ms (35.91%), Space: 16.9 MB (6.36%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+        
+        
+        letters = Counter(letters)
+        scr = defaultdict(int)
+        
+        for i in range(len(score)):
+            scr[chr(ord("a") + i)] = score[i]
+           
+        @cache
+        def dp(idx, path):
+            
+            if idx >= len(words):
+                return 0
+            
+            wd = Counter(words[idx])
+            total = 0
+            select = 0
+
+            for ltr in wd:
+                if wd[ltr] > letters[ltr]:
+                    break
+            
+            else:
+                
+                for ltr in wd:
+                    letters[ltr] -= wd[ltr]
+                    total += (scr[ltr] * wd[ltr])
+                  
+                pathSelect = [i for i in path]
+                pathSelect.append(idx)
+                pathSelect = tuple(pathSelect)
+                
+                select = dp(idx + 1, pathSelect) + total
+                
+                for ltr in wd:
+                    letters[ltr] += wd[ltr]
+                    
+            notSelect = dp(idx + 1, path)
+            
+            return max(select, notSelect)
+                    
+                 
+        return dp(0,())
+            
+        
+            
