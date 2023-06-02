21ccbfda25f3ea4065dc78ef43dfe706822ef699
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-06-02 15:12:56 +0300

    Time: 1495 ms (29.50%), Space: 17 MB (57.48%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,56 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        
+        parent = {}
+        size = {}
+        
+        def find(x):
+            nonlocal parent
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        
+        def union(x, y):
+            nonlocal parent
+            repX = find(x)
+            repY = find(y)
+            
+            if repX == repY:
+                return
+            
+            if size[repY] < size[repX]:
+                parent[repY] = repX
+                size[repX] += size[repY]
+                
+            else:
+                parent[repX] = repY
+                size[repY] += size[repX]
+                
+        
+        for i in range(len(stones)):
+            stones[i] = tuple(stones[i])
+            size[stones[i]] = 1
+            parent[stones[i]] = stones[i]
+        
+   
+        for i in range(len(stones)):
+            for j in range(i + 1, len(stones)):
+                cell1 = stones[i]
+                cell2 = stones[j]
+
+                if cell1[0] == cell2[0] or cell1[1] == cell2[1]:
+                    union(cell1, cell2)
+        
+        ans = 0
+        visited = set()
+        
+        for i in range(len(stones)):
+            pnt = find(stones[i])
+            if pnt not in visited:
+                ans += size[pnt] - 1
+                visited.add(pnt)
+                
+                
+        return ans
+            
