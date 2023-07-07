6e847180f23f0684c3d5d6819e2c1bbbece161a7
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-07-07 09:58:03 +0300

    Time: 426 ms (30.54%), Space: 18 MB (96.68%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,28 +2,78 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
         
-        
         ans = []
         comb = []
         
-        def recur(start):
-            
+        
+        def recur(s):
             if len(comb) == k:
                 ans.append(comb[:])
                 return
             
-            
-            for i in range(start, n + 1):
-                
+            for i in range(s, n + 1):
                 comb.append(i)
                 recur(i + 1)
                 comb.pop()
-        
-        
+                
         
         recur(1)
         
         return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+#         ans = []
+#         comb = []
+        
+#         def recur(start):
+            
+#             if len(comb) == k:
+#                 ans.append(comb[:])
+#                 return
+            
+            
+#             for i in range(start, n + 1):
+                
+#                 comb.append(i)
+#                 recur(i + 1)
+#                 comb.pop()
+        
+        
+        
+#         recur(1)
+        
+#         return ans
         
         
         
