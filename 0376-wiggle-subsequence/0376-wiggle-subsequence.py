09d5106b1b0dc25f34dffd4cb36fdc3ff464198e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-08-31 20:40:49 +0300

    Time: 47 ms (56.36%), Space: 16.3 MB (87.90%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -0,0 +1,39 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        
+        
+        if len(nums) == 1:
+            return 1
+        
+        count = 1
+        last = nums[0]
+        positive = None
+        
+        for i in range(1, len(nums)):
+            
+            curr = nums[i]
+            
+            if positive == None and curr - last != 0:
+                positive = (curr - last > 0)
+                last = curr
+                count = 2
+
+            elif curr - last > 0 and not positive:
+                positive = not positive
+                last = curr
+                count += 1
+                
+            elif curr - last < 0 and positive:
+                positive = not positive
+                last = curr
+                count += 1
+                
+            else:
+                last = curr
+                
+                
+        return count
+                
+                
+            
+            
