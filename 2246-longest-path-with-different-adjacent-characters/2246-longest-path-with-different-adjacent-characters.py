99f50a84bab47e137d9e4c9a129d451d42955cf6
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-09-01 20:40:14 +0300

    Time: 1338 ms (83.85%), Space: 157 MB (49.62%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -12,8 +12,7 @@ class Solution:
         def dfs(node):
             nonlocal ans
             if node not in adjList:
-                return [s[node], 1]
-            
+                return [s[node], 1]   
             
             last = 1
             for child in adjList[node]:
@@ -25,30 +24,6 @@ class Solution:
                 last = max(last, poss)
 
             return [s[node], last]  
-            
-            
-#             if len(adjList[node]) == 2:
-#                 left = dfs(adjList[node][0])
-#                 right = dfs(adjList[node][1])
-                
-#                 poss1 = left[1] + right[1] + 1 if left[0] != right[0] != s[node] else 1
-                
-#                 poss2 = left[1] + 1 if left[0] != s[node] else 1
-#                 poss3 = right[1] + 1 if right[0] != s[node] else 1
-                
-#                 ans = max(ans, poss1, poss2, poss3)
-                
-#                 return [s[node], max(poss2, poss3)]   
-                
-                
-#             elif len(adjList[node]) == 1:
-                
-#                 res = dfs(adjList[node][0])
-#                 poss = res[1] + 1 if res[0] != s[node] else 1
-                
-#                 ans = max(ans, poss)
-                
-#                 return [s[node], poss]
                 
                 
         dfs(0)
