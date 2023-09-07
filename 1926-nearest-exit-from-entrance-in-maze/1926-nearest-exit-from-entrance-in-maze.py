58923afaf763c02384f4ff1a88055483c9bbc4bc
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-09-07 10:26:04 +0300

    Time: 839 ms (31.34%), Space: 18 MB (26.15%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,37 +1,29 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         
-        
-        directions = [(0, 1),(0, -1),(-1, 0),(1, 0)]
-        
-        def isInbound(index):
-            row = index[0]
-            col = index[1]
-            
-            return 0 <= row < len(maze) and 0 <= col < len(maze[0])
-        
-        level = deque([entrance])
+        directions = [(0,1),(1,0),(-1,0),(0,-1)]
+        layer = deque([entrance])
         visited = set()
-        currLayer = 0
+        visited.add((entrance[0], entrance[1]))
         
+        def isInbound(idx):
+            return 0 <= idx[0] < len(maze) and 0 <= idx[1] < len(maze[0])
         
-        while level:
-            
-            currLayer += 1
-            for _ in range(len(level)):
-                
-                cell = level.popleft()
-                
+        steps = -1
+        while layer:
+            steps += 1
+            for _ in range(len(layer)):
+                cell = layer.popleft()
                 for direction in directions:
                     row = cell[0] + direction[0]
                     col = cell[1] + direction[1]
                     
-                    if isInbound((row, col)) and (row,col) not in visited and maze[row][col] == ".":
+                    if isInbound((row,col)) and maze[row][col] == '.' and (row, col) not in visited:
                         visited.add((row, col))
-                        level.append([row, col])
+                        layer.append([row, col])
                         
-                    elif not isInbound((row, col)) and  [cell[0], cell[1]] != entrance:
-                        return currLayer - 1
-                    
-                    
+                    elif not isInbound((row,col)) and [cell[0], cell[1]] != entrance :
+                        return steps 
+                        
         return -1
+        
