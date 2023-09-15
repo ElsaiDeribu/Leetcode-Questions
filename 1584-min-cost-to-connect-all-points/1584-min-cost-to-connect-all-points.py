d00616c4ebd3972e8d7c0d485cc4cfd012fe51cd
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-09-15 19:54:35 +0300

    Time: 3446 ms (19.52%), Space: 131.2 MB (49.35%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -2,62 +2,57 @@ class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         
         connections = []
-        parent = {i:i for i in range(len(points))} 
-        ans = 0
+        parent = {tuple(p):tuple(p) for p in points} 
         
         for i in range(len(points)):
             for j in range(i + 1, len(points)):
                 
-                p1 = points[i]
-                x1, y1 = p1[0], p1[1]
+                a = tuple(points[i])
+                ax = a[0]
+                ay = a[1]
                 
-                p2 = points[j]
-                x2, y2 = p2[0], p2[1]
+                b = tuple(points[j])
+                bx = b[0]
+                by = b[1]
                 
-                distance = abs(x1 - x2) + abs(y1 - y2)
+                distance = abs(ax - bx) + abs(ay - by)
                 
-                connections.append((distance, i, j))
-                
+                connections.append((distance,a,b))
+        
         connections.sort()
+        
+        
         
         def find(x):
             nonlocal parent
             
-            if parent[x] == x:
-                return x
-            
-            parent[x] = find(parent[x])
-            
-            return parent[x]
-        
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+                
+            return x
         
         def union(x, y):
             nonlocal parent
             
             repX = find(x)
-            repY = find((y))
-            
+            repY = find(y)
             
             if repX == repY:
                 return False
             
-            parent[repX] = parent[repY]
+            parent[repX] = repY
             
             return True
         
-    
-        for edge, p1, p2 in connections:
+        ans = 0
+        
+        for edge in connections:
+            if union(edge[1], edge[2]):
+                ans += edge[0]
             
-            if union(p1, p2):
-                ans += edge
-                
-                
         return ans
             
             
             
-            
-            
-            
-            
-            
+        
