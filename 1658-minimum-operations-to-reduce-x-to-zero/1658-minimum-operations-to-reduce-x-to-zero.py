b0c522879e455515f27e39b14da6a53cdd66968d
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-09-20 10:50:45 +0300

    Time: 1304 ms (5.38%), Space: 31 MB (29.84%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        
+         
+        pref = nums[:]
+        post = nums[:]
+        
+        for i in range(1, len(pref)):
+            pref[i] += pref[i - 1] 
+            
+        for i in range(len(pref) - 2, -1, -1):
+            post[i] += post[i + 1]
+        
+        post.append(0)
+        post = post[::-1]
+        
+        temp = [0]
+        temp.extend(pref)
+        pref = temp
+       
+           
+#         print(pref)
+#         print(post)
+        ans = float("inf")
+        
+        for i in range(len(nums)):
+            req = x - pref[i] 
+            possIdx = bisect_left(post, req)
+            # print(req, possIdx, i)
+            
+            if possIdx < len(post) and (possIdx + i) < len(post) and post[possIdx] == req:
+                ans = min(ans, i + possIdx)
+                
+            
+        return ans if ans != float("inf") else -1
+
+
+        
+        
