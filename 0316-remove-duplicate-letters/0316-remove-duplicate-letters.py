39b2b863d6f5d721b673bceb387e37f3fad01514
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-09-26 10:53:10 +0300

    Time: 50 ms (19.53%), Space: 16.2 MB (82.57%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,27 +1,27 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         
-        count = Counter(s)
-        stackElements = set()
-        stack = []
         
+        st = []
+        visited = set()
+        count = Counter(s)
         
         for i in range(len(s)):
             
-            if s[i] not in stackElements:
-                while stack and (stack[-1] > s[i] and count[stack[-1]] > 0):
-                    temp = stack.pop()
-                    stackElements.remove(temp)
+            if s[i] not in visited:
+                
+                while st and st[-1] >= s[i] and count[st[-1]] > 0:
+                    visited.remove(st.pop())
 
-             
-                stack.append(s[i])
+                st.append(s[i])
                 count[s[i]] -= 1
-                stackElements.add(s[i])
-
+                visited.add(s[i])
+                
             else:
                 count[s[i]] -= 1
                 
-        
                 
-        return ''.join(stack)
-        
+        return ''.join(st)
+
+
+            
