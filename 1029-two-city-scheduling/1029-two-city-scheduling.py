cb2feffc476d51b5c0f48ce7ef72b926c8c17376
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-10-03 15:33:01 +0300

    Time: 38 ms (95.92%), Space: 16.2 MB (75.18%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,25 +1,24 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         
-        ans = 0
+        
+        costs = sorted(costs, key = lambda x: abs(x[0] - x[1]), reverse = True)
         a = 0
         b = 0
         half = len(costs) // 2
+        ans = 0
         
-        costs.sort(key = lambda x : abs(x[0] - x[1]), reverse = True)
-        
-        for cost in costs:
-            if (cost[0] > cost[1] and  b < half) or (a == half and b < half):
-                ans += cost[1]
-                b += 1
-                
-            elif a < half or (b == half and a < half):
-                ans += cost[0]
+        for i in range(len(costs)):
+            costA = costs[i][0]
+            costB = costs[i][1]
+            
+            if costA < costB and a < half or b >= half:
                 a += 1
+                ans += costA
+                
+            else:
+                b += 1
+                ans += costB
                 
                 
         return ans
-                
-                
-                
-                
