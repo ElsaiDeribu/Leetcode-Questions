755ee24f80db0fca2a62cb02b27d205cac8d5b62
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-10-06 11:09:17 +0300

    Time: 44 ms (32.51%), Space: 16.4 MB (27.37%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -2,21 +2,19 @@ class Solution:
     def integerBreak(self, n: int) -> int:
         
         
-        @cache
-        def dp(rem, idx):
-            
-            if rem == 0:
-                return 1
-            
-            res = float(-inf)
-            end =  rem + 1 if idx != 0 else rem
-            for i in range(1, end):
-                
-                res =  max(res, i * dp(rem - i, idx + 1))
-                          
-                          
-            return res
-                         
-                          
-        return dp(n, 0)
+        if n <= 3:
+            return n - 1
+        
+        rem = n % 3
+        temp = (n // 3)
+        
+        if rem == 0:
+            return 3 ** temp
+        
+        if rem == 1:
+            return (3 ** (temp - 1)) * 4
+        
+        
+        return (3 ** temp) * 2
+        
                 
