8231cb464b469f67a245d2397bb52ea052467a49
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2023-10-23 11:56:01 +0300

    Time: 1041 ms (62.80%), Space: 26.8 MB (81.71%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -1,47 +1,31 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         
-        st = []
-        ranges = defaultdict(list)
+        l = r = k 
+        ans = nums[k]
+        smallest = nums[k]
         
-        
-        for i in range(len(nums) - 1, -1, -1):
-            while st and nums[st[-1]] > nums[i]:
-                num = st.pop()
-                ranges[num].append(i + 1)
+        while l > 0 or r < len(nums) - 1:
             
-            st.append(i)
-            
+            if l == 0 or (r < len(nums) - 1 and nums[l - 1] < nums[r + 1]):
+                r += 1
+                smallest = min(smallest, nums[r])
                 
-        for item in st:
-            ranges[item].append(0)
-            
-        
-        st = []
-        
-        for i in range(len(nums)):
-            
-            while st and nums[st[-1]] > nums[i]:
-                num = st.pop()
-                ranges[num].append(i - 1)
-            
-            st.append(i)
-            
-        
-        for item in st:
-            ranges[item].append(len(nums) - 1)
-            
-        
-        ans = float("-inf")
-            
-        for element in ranges:
-            
-            left , right = ranges[element]
-            idx = element
-            
-            if left <= k <= right:
-                ans = max(ans, nums[idx] * (right- left + 1))
+            else:
+                l -= 1 
+                smallest = min(smallest, nums[l])
                 
+                
+            ans = max(ans, smallest * (r - l + 1) )
+            
+            
         return ans
+                
+            
+            
+        
+        
+        
+
         
         
