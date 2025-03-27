65aae1b486f627a5e58e718747f1370cc364c8a1
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-03-27 17:02:39 -0400

    Time: 19 ms (37.77%) | Memory: 22.7 MB (12.80%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,8 +4,12 @@ class Solution:
         dic = defaultdict(list)
 
         for word in strs:
-           
-            dic[''.join(sorted(word))].append(word)
+            temp = [0] * 26
+
+            for l in word:
+                temp[ord(l) - 97] += 1
+
+            dic[tuple(temp)].append(word)
 
         return [dic[key] for key in dic ]
 
