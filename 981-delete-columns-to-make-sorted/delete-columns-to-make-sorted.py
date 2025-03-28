cb3ba37d79519c043aed174a626812123b6334ab
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-03-28 11:33:42 -0400

    Time: 86 ms (20.05%) | Memory: 18.2 MB (70.84%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -1,18 +1,16 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
 
-        grid = [["" for _ in range(len(strs))] for _ in range(len(strs[0]))]
-
-        for r in range(len(strs)):
-            for c in range(len(strs[r])):
-                grid[c][r] = strs[r][c]
-
         count = 0
 
-        for row in grid:
-            if not row == sorted(row):
-                count += 1
-
+        for c in range(len(strs[0])):
+            for r in range(len(strs)):
+                prev = r - 1 if r > 0 else 0
+                if strs[prev][c] > strs[r][c]:
+                    count +=1
+                    break
+                
+            
         return count
 
 
