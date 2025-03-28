d73e8329a82653bcf7235608463714705e867d69
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-03-28 15:47:01 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (33.21%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+
+
+        def helper(n, k):
+            if n == 1:
+                return 0
+
+            return (helper(n - 1, k) + k) % n
+
+        return helper(n, k) + 1
+
+
+
+        
