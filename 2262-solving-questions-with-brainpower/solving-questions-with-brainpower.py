f742740695e3163bb7d901353ce9d4d6975b0a5d
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-04-02 17:56:23 -0400

    Time: 78 ms (67.61%) | Memory: 60.9 MB (64.23%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,13 +1,18 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         
-        @cache
-        def dp(idx):
+        n = len(questions)
+        cache = [0] * n
 
-            if idx >= len(questions):
-                return 0
+        for i in range(n - 1, -1, -1):
 
-            return max(dp(idx + questions[idx][1] + 1) + questions[idx][0], dp(idx + 1))
+            points, brain = questions[i]
+            
+            next_i = i + 1 + brain
 
+            take = points + (cache[next_i] if next_i < n else 0  )
+            not_take = cache[i + 1] if i + 1 < n else 0
 
-        return dp(0) 
+            cache[i] = max(take, not_take)
+
+        return cache[0]
