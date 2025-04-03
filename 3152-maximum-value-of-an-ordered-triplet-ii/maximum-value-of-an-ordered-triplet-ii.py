9cacda690cf02ff96a2bfa6d980f1d409bd531ff
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-04-03 07:20:35 -0400

    Time: 268 ms (10.92%) | Memory: 30.2 MB (26.28%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -8,10 +8,7 @@ class Solution:
 
         for i in range(1, n):
             pref[i] = max(pref[i], pref[i - 1])
-
-        for i in range(n - 2, -1, -1):
-            suf[i] = max(suf[i], suf[i + 1])
-
+            suf[n - i - 1] = max(suf[n - i - 1], suf[n - i])
 
         for i in range(1, n - 1):
             best = (pref[i - 1] - nums[i]) * suf[i + 1]
