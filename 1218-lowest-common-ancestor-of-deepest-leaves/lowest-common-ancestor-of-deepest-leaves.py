a67419ef768a8bdf306e19ac8ef2e3656d3cd3ff
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-04-04 16:02:27 -0400

    Time: 3 ms (57.35%) | Memory: 18.2 MB (71.99%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -10,21 +10,18 @@ class Solution:
 
         def dfs(node, depth):
             if not node:
-                return [None, depth]
+                return None, depth
 
-            left = node.left if node.left else None
-            right = node.right if node.right else None
-
-            left_n, dl = dfs(left, depth + 1)
-            right_n, dr = dfs(right, depth + 1)
+            left_n, dl = dfs(node.left, depth + 1)
+            right_n, dr = dfs(node.right, depth + 1)
 
             if dl == dr:
-                return [node, dl]
+                return node, dl
 
             elif dl > dr:
-                return [left_n, dl]
+                return left_n, dl
 
-            return [right_n, dr]
+            return right_n, dr
 
         return dfs(root, 0)[0]
 
