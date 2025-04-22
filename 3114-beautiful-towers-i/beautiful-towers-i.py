142f12a235e130a7abc568554e4e975dccb8b435
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-04-22 10:10:30 -0400

    Time: 1023 ms (38.10%) | Memory: 18 MB (58.97%) - LeetSync

--- 3114-beautiful-towers-i/beautiful-towers-i.py
+++ 3114-beautiful-towers-i/beautiful-towers-i.py
@@ -1,11 +1,12 @@
 class Solution:
     def maximumSumOfHeights(self, heights: List[int]) -> int:
 
-        def helper(arr):
+        def helper(left, right, drctn):
             cst = 0
             st = []
 
-            for num in arr:
+            for i in range(left, right, drctn):
+                num = heights[i]
                 if st and st[-1] < num:
                     cst += (num - st[-1])
                     st.append(st[-1])
@@ -18,8 +19,8 @@ class Solution:
         min_cst = float("inf")
 
         for i in range(len(heights)):
-            cst_l = helper(heights[i::-1])
-            cst_r = helper(heights[i + 1:])
+            cst_l = helper(i, -1, -1)
+            cst_r = helper(i + 1, len(heights), 1)
 
             min_cst = min(min_cst, cst_l + cst_r)
 
