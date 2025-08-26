a3b7565968e639658eb30d8c6f039a434d9f7d44
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-08-26 00:50:17 -0500

    Time: 241 ms (78.99%) | Memory: 40.3 MB (98.74%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -3,26 +3,11 @@ class Solution:
 
         potions.sort()
 
-        def check(s):
-
-            l = 0
-            r = len(potions) - 1
-
-            while l <= r:
-
-                m = (l + r) // 2
-
-                if potions[m] *  s < success:
-                    l = m + 1
-                else:
-                    r = m - 1
-
-            return l
-
+      
 
         for i in range(len(spells)):
 
-            idx = check(spells[i])
+            idx = bisect_left(potions, success/spells[i])
             spells[i] = len(potions) - idx
 
         return spells
