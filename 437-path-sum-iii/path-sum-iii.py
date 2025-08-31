58622fba02f04455a88ed7c48d0fefcefdce02b7
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-08-31 14:31:03 -0500

    Time: 3 ms (89.05%) | Memory: 18.5 MB (32.44%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+
+        
+        def dfs(pref, total, node):
+
+            if not node:
+                return 0
+
+            total += node.val
+            pref[total] += 1
+            
+            req = total - targetSum
+
+
+            res_l = dfs(pref, total , node.left)
+            res_r = dfs(pref, total , node.right)
+
+            pref[total] -= 1
+
+
+            return res_l + res_r + pref[req]
+
+        pref = defaultdict(int)
+        pref[0] = 1
+
+
+
+        return dfs(pref, 0, root)
+
+
+
