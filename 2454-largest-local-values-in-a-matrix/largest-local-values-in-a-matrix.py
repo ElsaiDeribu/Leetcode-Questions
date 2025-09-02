f73238dd9307d4de71b20fc31130c44c73871d4c
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-09-02 13:12:57 -0500

    Time: 44 ms (59.05%) | Memory: 19.1 MB (6.80%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -1,14 +1,34 @@
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
 
-    
-        for r in range(1, len(grid) - 1):
-            for c in range(1, len(grid[0]) - 1):
+        dir = [(0,1),(1,0),(0,-1),(-1,0),(1,1),(-1,1),(1,-1),(-1,-1)]
 
-                for i in range(r - 1, r + 2):
-                    for j in range( c - 1, c + 2):
+        updates = []
 
-                        grid[r - 1][c - 1] = max(grid[r - 1][c - 1], grid[i][j])
         
+        for r in range(1, len(grid) - 1):
+            for c in range(1, len(grid) - 1):
+                curr = grid[r][c]
 
-        return [grid[r][:len(grid[0]) - 2 ] for r in range(len(grid) - 2) ]
+                for d_r, d_c in dir:
+                    row, col = r + d_r, c + d_c
+
+                    curr = max(curr, grid[row][col])
+
+                updates.append((r,c,curr))
+
+        for r, c , val in updates:
+            grid[r][c] = val
+            
+        grid = grid[1:len(grid) - 1]
+
+        for i in range(len(grid)):
+            grid[i] = grid[i][1:len(grid[i]) - 1]
+
+
+
+        return grid
+
+
+
+        
