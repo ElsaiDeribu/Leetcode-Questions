4698d066157688008596e9a897226fc41b98638e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-09-07 13:02:24 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (12.75%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -2,19 +2,19 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
 
         @cache
-        def dp(curr, res):
+        def dp(curr):
 
             if curr >= len(nums):
-                return res
+                return 0
 
 
             # take
-            res1 = dp(curr + 2, res + nums[curr])
+            res1 = dp(curr + 2) + nums[curr]
 
             # not take
-            res2 = dp(curr + 1, res)
+            res2 = dp(curr + 1)
 
             return max(res1, res2)
 
 
-        return dp(0,0)
+        return dp(0)
