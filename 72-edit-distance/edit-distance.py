9eff65ccb28b1a0e229aab4d7f2020489ac10665
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-09-22 20:29:55 -0500

    Time: 39 ms (84.73%) | Memory: 20.1 MB (69.01%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,7 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        
+
         @cache
         def dp(w1, w2):
             if w1 == len(word1):
@@ -10,21 +10,14 @@ class Solution:
             if w2 == len(word2):
                 return len(word1) - w1
 
-
             if word1[w1] == word2[w2]:
                 return dp(w1 + 1, w2 + 1) 
 
-            # insert
-            res1 = dp(w1, w2 + 1) + 1
-
-            # delete
-            res2 = dp(w1 + 1, w2) + 1
-            
-            # replace
-            res3 = dp(w1 + 1, w2 + 1) + 1
-
-
-            return min(res1, res2, res3)
+            return 1 + min(
+                dp(w1, w2 + 1), # insert
+                dp(w1 + 1, w2), # delete
+                dp(w1 + 1, w2 + 1) # replace
+                )
 
 
         return dp(0,0)
