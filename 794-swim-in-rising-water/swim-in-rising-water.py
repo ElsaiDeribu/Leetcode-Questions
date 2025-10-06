877b5e4b223e555f813dfaecb966cc35705c5f3e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-06 16:11:43 -0500

    Time: 27 ms (66.79%) | Memory: 18.4 MB (72.30%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -15,20 +15,19 @@ class Solution:
 
         while heap:
 
-            for _ in range(len(heap)):
-                val_min, row_min, col_min = heapq.heappop(heap)
+            val_min, row_min, col_min = heapq.heappop(heap)
 
-                ans = max(ans, val_min)
+            ans = max(ans, val_min)
 
-                for r, c in direc:
-                    row_new, col_new = row_min + r, col_min + c
+            for r, c in direc:
+                row_new, col_new = row_min + r, col_min + c
 
-                    if inbound(row_new, col_new) and (row_new, col_new) not in visited:
-                        heapq.heappush(heap, (grid[row_new][col_new],row_new,col_new))
-                        visited.add((row_new, col_new))
+                if inbound(row_new, col_new) and (row_new, col_new) not in visited:
+                    heapq.heappush(heap, (grid[row_new][col_new],row_new,col_new))
+                    visited.add((row_new, col_new))
 
-                        if (row_new, col_new) == (len(grid) - 1, len(grid[0]) - 1):
-                            return max(ans, grid[row_new][col_new] )
+                    if (row_new, col_new) == (len(grid) - 1, len(grid[0]) - 1):
+                        return max(ans, grid[row_new][col_new] )
 
 
 
