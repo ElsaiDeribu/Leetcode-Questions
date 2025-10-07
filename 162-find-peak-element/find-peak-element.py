940e6d5d774ac5d37dc15f86f59227a3e0938a86
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-07 18:44:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.56%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,25 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
-        l = 0
-        r = len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         while l <= r:
+            m = l + (r - l ) // 2 
 
-            m = l + (r - l) // 2
-
-            if  m < len(nums) - 1 and nums[m] < nums[m + 1] :
+            if m + 1 < len(nums) and nums[m] < nums[m + 1]:
                 l = m + 1
 
-            elif m > 0 and  nums[m - 1] > nums[m]:
+            else:
                 r = m - 1
 
-            else:
-                return m
 
-
-
-
-
-
+        return l
         
