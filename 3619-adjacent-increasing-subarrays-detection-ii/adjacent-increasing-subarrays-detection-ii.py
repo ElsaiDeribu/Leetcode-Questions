e8b852dfb95a47810625b0e53667009d5e3c4073
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-14 21:31:18 -0400

    Time: 1429 ms (49.68%) | Memory: 47.1 MB (18.06%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.py
@@ -1,22 +1,21 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
 
-        inc = [1] * len(nums)
-
-        for i in range(1, len(nums)):
-            if nums[i - 1] < nums[i]:
-                inc[i] = inc[i - 1] + 1
-
-        print(inc)
+        prev_run_len = 0   
+        curr_len = 1      
         ans = 1
-        for i in range(len(nums)):
-            k = inc[i]
 
-            if i - k >= 0 and inc[i] <= inc[i - k]:
-                ans = max(ans, k)
-                
-            ans = max(ans, k//2)
+        for i in range(1, n):
+            if nums[i - 1] < nums[i]:
+                curr_len += 1
+            else:
+                prev_run_len = curr_len
+                curr_len = 1
 
+            if prev_run_len >= curr_len:
+                ans = max(ans, curr_len)
+
+            ans = max(ans, curr_len // 2)
 
         return ans
-        
