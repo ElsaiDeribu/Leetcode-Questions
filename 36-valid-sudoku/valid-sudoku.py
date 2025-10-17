e7d0eaf067a63316c7813fce6373cd7760f6e669
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-17 16:21:15 -0400

    Time: 2 ms (84.76%) | Memory: 17.7 MB (69.33%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,35 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+
         rows = defaultdict(set)
         cols = defaultdict(set)
         box = defaultdict(set)
 
 
+
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if board[r][c] != ".":
-                    if board[r][c] in rows[r] or board[r][c] in cols[c]:
+                val = board[r][c]
+                if  val != ".":
+                    if val in rows[r] or val in cols[c]:
                         return False
 
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
+                    row = r // 3
+                    col = c // 3
+
+                    if val in box[(row,col)]:
+                        return False
+
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    box[(row,col)].add(val)
 
 
-        for r in range(1, len(board), 3):
-            for c in range(1, len(board[0]), 3):
-
-                for dr in range(-1, 2):
-                    for dc in range(-1, 2):
-                        row = dr + r
-                        col = dc + c
-
-                        if board[row][col] != ".":
-
-                            if board[row][col] in box[(r,c)]:
-                                return False
-
-                            box[(r,c)].add(board[row][col])
-
+       
 
         return True
 
