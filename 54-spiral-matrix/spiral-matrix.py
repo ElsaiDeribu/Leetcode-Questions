f6dbe68b79ed657bb9a7041cae53005682b6b624
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-17 15:32:41 -0400

    Time: 2 ms (2.93%) | Memory: 17.8 MB (38.42%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,49 +1,33 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
 
+
+        l, r = 0, len(matrix[0]) - 1
+        t, b = 0, len(matrix) - 1
         ans = []
 
-        l, t = 0, 0
-        r, b = len(matrix[0]) - 1, len(matrix) - 1
+        while l <= r and t <= b: 
 
-        d = "r"
+            for i in range(l, r + 1): 
+                ans.append(matrix[t][i])
+            t += 1
 
-        while l <= r and t <= b:
+            for i in range(t, b + 1):
+                ans.append(matrix[i][r])
+            r -= 1
 
-            
-            if d == "r":
-
-                for i in range(l, r + 1):
-                    ans.append(matrix[t][i])
-
-                t += 1
-                d = "d"
-
-            elif d == "d":
-                for i in range(t, b + 1):
-                    ans.append(matrix[i][r])
-
-                r -= 1
-                d = "l"
-
-            elif d == "l":
+            if t <= b:
                 for i in range(r, l - 1, -1):
                     ans.append(matrix[b][i])
-
                 b -= 1
-                d = "u"
 
-
-            else:
+            if l <= r:
                 for i in range(b, t - 1, -1):
                     ans.append(matrix[i][l])
-
                 l += 1
-                d = "r"
+
+
 
 
         return ans
-                
-
-
         
