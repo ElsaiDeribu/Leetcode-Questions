3d22aac3346030b4104e0144bf75df23b1d97e31
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-10-25 15:34:22 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (20.41%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,16 +1,19 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
 
-        citations.sort()
+        n = len(citations)
+        count = [0] * (n + 1)
 
+        for idx, val in enumerate(citations):
+            count[min(n, val)] += 1
+
+        total = 0
         ans = 0
 
-        for val in range(1001):
-            idx = bisect_left(citations, val)
+        for i in range(n, -1, -1):
+            total += count[i]
+            if total >= i:
+                return i
 
-            if len(citations) - idx >= val:
-                ans = val
 
-
-        return ans
         
