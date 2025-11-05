0003632deabc8467ae68610955b5064a527021b1
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-11-04 22:21:31 -0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (78.24%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,20 +3,45 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        rows = set()
-        cols = set()
+        first_row_has_0 = False
+        first_col_has_0 = False
 
         for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+            if matrix[r][0] == 0:
+                first_col_has_0 = True
+                break
+
+        for c in range(len(matrix[0])):
+            if matrix[0][c] == 0:
+                first_row_has_0 = True
+
+
+        for r in range(1, len(matrix)):
+            for c in range(1, len(matrix[0])):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
 
-        for r in rows:
+        for r in range(1, len(matrix)):
+            if matrix[r][0] == 0:
+                for c in range(len(matrix[0])):
+                    matrix[r][c] = 0
+
+        for c in range(1, len(matrix[0])):
+            if matrix[0][c] == 0:
+                for r in range(len(matrix)):
+                    matrix[r][c] = 0
+
+
+        if first_row_has_0:
             for c in range(len(matrix[0])):
-                matrix[r][c] = 0
+                matrix[0][c] = 0
 
-        for r in range(len(matrix)):
-            for c in cols:
-                matrix[r][c] = 0
+        if first_col_has_0:
+            for r in range(len(matrix)):
+                matrix[r][0] = 0
+
+
+        
+
+
