8859e227dd4d6c85627bbd0031ebc0fe26382cfb
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-11-06 15:23:23 -0500

    Time: 3 ms (72.74%) | Memory: 18.1 MB (14.42%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -7,32 +7,40 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
 
-        num1 = []
-        num2 = []
+        head1 = l1
+        head2 = l2
 
-        h1, h2 = l1, l2
+        num1 = 1
 
-        while h1:
-            num1.append(str(h1.val))
-            h1 = h1.next
+        while head1:
 
-        while h2:
-            num2.append(str(h2.val))
-            h2 = h2.next
+            num1 *= 10
+            num1 += head1.val
+            head1 = head1.next
 
-        num1 = int(''.join(num1[::-1]))
-        num2 = int(''.join(num2[::-1]))
+        num2 = 1
+        while head2:
 
-        num = str(num1 + num2)[::-1]
+            num2 *= 10
+            num2 += head2.val
+            head2 = head2.next
 
+        num1 = int(str(num1)[1:][::-1])
+        num2 = int(str(num2)[1:][::-1])
 
+        num3 = str(num1 + num2)
         head = l1
-
-        for i, n in enumerate(num):
-            head.val = int(n)
-            if not head.next and i < len(num) - 1:
+        print(num1, num2, num3)
+        for i in range(len(num3) - 1, -1, -1):
+            if not head.next and i >  0:
                 head.next = ListNode()
+
+            head.val = int(num3[i])
             head = head.next
 
+
+
         return l1
+
+
 
