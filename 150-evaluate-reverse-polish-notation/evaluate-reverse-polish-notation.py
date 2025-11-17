625bac7c755a62578d775a98d225a89f20cdaba8
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-11-17 16:37:33 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (87.87%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,20 +1,22 @@
+import operator
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        ops = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": lambda a, b: int(a / b)
+        }
         
-        st = []
-
-        for token in tokens:
-            if token in ["+", "-", "*", "/"]:
-                op1 = st.pop()
-                op2 = st.pop()
-                if token == "/":
-                    st.append(str(int(op2) / int(op1)).split('.')[0])
-
-                else:   
-                    st.append(str(eval(op2 + token + op1)))
-
+        stack = []
+        
+        for t in tokens:
+            if t in ops:
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(ops[t](a, b))
             else:
-                st.append(token)
-
-
-        return int(st[-1])
+                stack.append(int(t))
+        
+        return stack[-1]
