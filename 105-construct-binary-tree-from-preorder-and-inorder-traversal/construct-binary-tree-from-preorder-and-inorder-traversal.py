3f052847ce7cba23f7c82e12dd5f77aa127a4f8e
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-12-04 16:07:30 -0500

    Time: 3 ms (87.45%) | Memory: 19.6 MB (55.25%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,41 +6,33 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-
+        
+        dic = {val:idx for idx, val in enumerate(inorder)}
 
         def dfs(pre_start, pre_end, in_start, in_end):
-
             if pre_start > pre_end or in_start > in_end:
                 return
 
             node_val = preorder[pre_start]
             node = TreeNode(node_val)
-
-            i = in_start
-            while inorder[i] != node_val:
-                i += 1
-
-            left_len = i - in_start
+            
+            left_len = dic[node_val] - in_start
 
             node.left = dfs(
-                pre_start + 1,
-                pre_start + left_len,
-                in_start, 
-                i - 1
-                 )
+                            pre_start + 1,
+                            pre_start + left_len,
+                            in_start,
+                            dic[node_val] - 1
+                            )
 
             node.right = dfs(
-                pre_start + left_len + 1,
-                pre_end,
-                i + 1,
-                in_end
-                )
+                            pre_start + left_len + 1,
+                            pre_end,
+                            dic[node_val] + 1,
+                            in_end
+                            )
 
             return node
 
 
         return dfs(0, len(preorder) - 1, 0, len(inorder) - 1)
-
-
-
-        
