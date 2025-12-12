5fc256a09ae685609a48c7cafbd1bc4d4cd06e00
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-12-11 22:10:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.34%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,14 +1,12 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
+        # TC: O(log_5 n)  - n divided by 5 each iteration
+        # SC: O(1)        - only constant extra space used
 
         ans = 0
 
         while n:
-
             ans += n // 5
             n //= 5
 
         return ans
-
-        
-      
