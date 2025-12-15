6ccf69154b694a7511b0dff24916e20feadc5c1d
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-12-15 13:13:32 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.10%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,41 +1,22 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
 
-        dic = {
-            "2":["a", "b", "c"],
-            "3":["d", "e", "f"],
-            "4":["g", "h", "i"],
-            "5":["j", "k", "l"],
-            "6":["m", "n", "o"],
-            "7":["p", "q", "r","s"],
-            "8":["t", "u","v"],
-            "9":["w", "x","y","z"], 
-        }
+
+        letter_comb = {"2":"abc", "3": "def", "4": "ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
 
         ans = []
 
-        def recur(idx, arr):
-
-            if idx == len(digits):
-                temp =''.join(arr)
-                ans.append(temp) if temp else None
+        def dfs(comb, idx):
+            if len(comb) == len(digits):
+                ans.append(''.join(comb))
                 return
 
-            for l in dic[digits[idx]]:
-                arr.append(l)
-                recur(idx + 1, arr)
-                arr.pop()
+            for letter in letter_comb[digits[idx]]:
+                dfs(comb + [letter], idx + 1)
 
-            
-        recur(0, [])
+
+        dfs([], 0)
 
         return ans
-
-
-
-
-
-
-
-
+            
         
