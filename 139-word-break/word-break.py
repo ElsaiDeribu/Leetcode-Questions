dc97a01c89de492201cfc8316c60cd5e49c0fc51
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2025-12-31 01:18:58 -0500

    Time: 3 ms (70.21%) | Memory: 17.3 MB (97.81%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        wordDict = set(wordDict)
+
+        @cache
+        def dp(idx):
+            if idx >= len(s):
+                return True
+
+            res = False
+            for i in range(idx, len(s)):
+                if s[idx:i + 1] in wordDict:
+                    res = res or dp(i + 1)
+
+            return res
+                
+
+        return dp(0)
