72f669aef241a4aa7fc14ddc52b0e5db72b9acbe
Elsai Deribu <91674971+ElsaiDeribu@users.noreply.github.com>
2026-02-19 12:06:32 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (28.17%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -7,14 +7,14 @@ class Solution:
         while l < r:
             m = (l + r) // 2
 
-            if nums[m] == target:
-                return m
-
             if nums[m] < target:
                 l = m + 1
 
             else:
                 r = m
         
+        if l < len(nums) and nums[l] == target:
+            return l
+
         return -1
     
