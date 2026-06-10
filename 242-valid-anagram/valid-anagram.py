3c3090f16372b22299f5ed52fe67a16580d1f8c1
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-09 22:26:32 -0400

    Time: 11 ms (76.29%) | Memory: 19.4 MB (75.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        s_chrs = [0] * 26
+        t_chrs = [0] * 26
 
-        return Counter(s) == Counter(t)
+        for character in s:
+            s_chrs[ord(character) - 97] += 1
+
+        for character in t:
+            t_chrs[ord(character) - 97] += 1
+        
+
+        return s_chrs == t_chrs
         
