510fa811939279553e9680661a041fdc30bc1c27
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-09 23:03:54 -0400

    Time: 13 ms (51.18%) | Memory: 23.7 MB (32.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-
-        dic = defaultdict(list)
+           
+        group = defaultdict(list)
 
         for word in strs:
-            dic[str(sorted(list(word)))].append(word) 
+
+            chrs = [0] * 26
+            for c in word:
+                chrs[ord(c) - 97] += 1
+
+            group[tuple(chrs)].append(word)
 
 
-        return list(dic.values())
+        return list(group.values())
+
+        
