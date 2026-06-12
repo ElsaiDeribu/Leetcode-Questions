61d0b72ac5a51f7d1447c99110376fc6264a9648
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-12 14:58:42 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (27.68%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,24 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
-        map = {"(":")", "{":"}", "[":"]"}
         st = []
+        pairs = {"[":"]", "{":"}", "(":")"}
 
 
-        for idx, val in enumerate(s):
+        for char in s:
 
-            if val in map:
-                st.append(val)
-
-            else:
-                if st and  map[st[-1]] == val:
+            if char not in pairs:
+                if st and pairs[st[-1]] == char:
                     st.pop()
-
                 else:
                     return False
+            else:
+                st.append(char)
 
-        
-        return True if not st else False
-                
-                
-        
+
+        return False if st else True
