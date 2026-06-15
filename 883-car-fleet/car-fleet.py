d658661d424868e64e1234dcb8f7abb8826bbef4
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-15 15:22:47 -0400

    Time: 191 ms (38.09%) | Memory: 41.8 MB (36.98%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -3,14 +3,14 @@ class Solution:
         
 
         res = sorted(list(zip(position, speed)))
-        
-        for idx, (pos, s) in enumerate(res):
-            res[idx] = (target - pos) / s
-
         st = []
-        for time in res:
+
+        for pos, s in res:
+
+            time = (target - pos) / s
             while st and st[-1] <= time:
                 st.pop()
+                
             st.append(time)
 
         return len(st)
