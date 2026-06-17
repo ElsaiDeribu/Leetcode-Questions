caf79bdaf91d2516f7890b6683b0dceda8406167
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-17 14:29:29 -0400

    Time: 59 ms (87.01%) | Memory: 19.9 MB (12.25%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -6,24 +6,26 @@ class Solution:
         t = Counter(t)
         ans = float("inf")
         left, right = None, None
+        have = 0
+        need = len(t)
         l = 0
-
-        def is_valid():
-            for key, val in t.items():
-                if window[key] < val:
-                    return False
-            return True
-
 
         for r in range(len(s)):
 
             window[s[r]] += 1
-            while is_valid():
+
+            if window[s[r]] == t[s[r]]:
+                have += 1
+
+            while have == need:
                 if r - l + 1 < ans:
                     ans = r - l + 1
                     left, right = l, r
 
                 window[s[l]] -= 1
+                if s[l] in t and window[s[l]] < t[s[l]]:
+                    have -= 1
+                    
                 l += 1
 
         
