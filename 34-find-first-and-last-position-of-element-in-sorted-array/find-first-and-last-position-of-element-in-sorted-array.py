1a99f253e290c1010ebbb7508d034f930ebaca5c
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-18 15:47:52 -0400

    Time: 0 ms (100.00%) | Memory: 20.8 MB (7.41%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
 
         left = bisect_left(nums, target)
 
-        if not (left < len(nums) and nums[left] == target):
+        if left == len(nums) or nums[left] != target:
             return [-1, -1]
 
         right = bisect_right(nums, target) - 1
