574b3b2fbf23aa80c23b409cba499e78c443ce18
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-18 12:50:58 -0400

    Time: 51 ms (94.59%) | Memory: 19.7 MB (73.69%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -4,32 +4,30 @@ class Solution:
 
         window = defaultdict(int)
         t = Counter(t)
-        ans = float("inf")
+        have, need = 0, len(t)
         left, right = None, None
-        have = 0
-        need = len(t)
         l = 0
+
 
         for r in range(len(s)):
 
             window[s[r]] += 1
-
             if window[s[r]] == t[s[r]]:
                 have += 1
 
             while have == need:
-                if r - l + 1 < ans:
-                    ans = r - l + 1
-                    left, right = l, r
+                if (left == None) or r - l < right - left:
+                    left, right  = l, r
 
                 window[s[l]] -= 1
-                if s[l] in t and window[s[l]] < t[s[l]]:
+                if window[s[l]] < t[s[l]]:
                     have -= 1
-                    
                 l += 1
 
-        
-        return s[left:right + 1] if left != None else ""
+        return s[left: right + 1] if left != None else ""
 
+
+
+        
 
         
