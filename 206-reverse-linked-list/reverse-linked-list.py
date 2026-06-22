6fc5ed8f3b63a552753ec51d0cd551d211b82875
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-22 15:21:58 -0400

    Time: 0 ms (100.00%) | Memory: 20.5 MB (27.06%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -10,7 +10,7 @@ class Solution:
         prev, curr = None, head
 
         while curr:
-            curr.next, curr, prev = prev, curr.next, curr
+            prev, curr.next, curr,  = curr, prev, curr.next, 
 
 
         return prev
