23f5bf9774c4767991f0501de08379e4a91b262e
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-06-29 12:53:16 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.55%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,28 +6,25 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
-
+        
 
         def dfs(node1, node2):
 
-            if not node1 and node2:
-                return False
+            if (node1 and not node2) or (node2 and not node1): return False
 
-            if node1 and not node2:
-                return False
+            if not node1 and not node2: return True
 
-            if not node1 and not node2:
-                return True
-
-            if node1.val != node2.val:
-                return False
+            if node1.val != node2.val: return False
 
             left = dfs(node1.left, node2.left)
             right = dfs(node1.right, node2.right)
+
 
             return left and right
 
 
         return dfs(p, q)
-    
+        
+
+
+            
