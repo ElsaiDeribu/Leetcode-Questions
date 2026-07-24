dcf19291b6f2186d28ca17ad32c2fec693de2254
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-07-24 16:58:22 -0400

    Time: 3 ms (74.87%) | Memory: 19.3 MB (78.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,40 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-        deq = deque([])
-        dir = [(0,1),(0,-1),(1,0),(-1,0)]
-        visited = set()
-        mins = 0
-        fr_cnt = 0
-
-
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 2:
-                    visited.add((r,c))
-                    deq.append((r,c))
-
-                elif grid[r][c] == 1:
-                    fr_cnt += 1
-
-        def isinbound(r,c):
-            return 0 <= r < len(grid) and 0 <= c < len(grid[0])
         
+        deq = deque([])
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+
+        def is_inbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 2:
+                    deq.append((row, col))
+
+        time = -1
         while deq:
-
             for _ in range(len(deq)):
-                r, c = deq.popleft()
 
-                for d_r, d_c in dir:
-                    row, col = r + d_r, c + d_c
+                row, col = deq.popleft()
 
-                    if isinbound(row,col) and (row, col) not in visited and grid[row][col] == 1:
-                        fr_cnt -= 1
-                        visited.add((row, col))
-                        deq.append((row,col))
-            if deq:
-                 mins += 1
-
+                for dr, dc in dirs:
+                    new_row = dr + row
+                    new_col = dc + col
+                    if is_inbound(new_row, new_col) and grid[new_row][new_col] == 1:
+                        grid[new_row][new_col] = 2
+                        deq.append((new_row, new_col))
+            time += 1
 
 
-        return -1 if fr_cnt > 0 else mins
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    return -1
+
+
+        return max(time, 0)
