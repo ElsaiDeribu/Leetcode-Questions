b37de09c89757736908bf36ffc64299cf2b204bf
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-07-27 12:59:31 -0400

    Time: 37 ms (11.17%) | Memory: 29.8 MB (7.94%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -10,7 +10,7 @@ class Solution:
             return 0 <= row < len(board) and 0 <= col < len(board[0])
 
         
-        def check(row, col):
+        def visit(row, col):
             
             visited.add((row, col))
             
@@ -18,12 +18,20 @@ class Solution:
                 new_row = dr + row
                 new_col = dc + col
 
-                if not is_inbound(new_row, new_col): return False
+                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O" and (new_row, new_col) not in visited:
+                    visit(new_row, new_col)
+                    
 
-                if board[new_row][new_col] == "O" and (new_row, new_col) not in visited:
-                    if not check(new_row, new_col): return False
+        for row in [0, len(board) - 1]:
+            for col in range(len(board[0])):
+                if board[row][col] == "O":
+                    visit(row, col)
 
-            return True
+
+        for row in range(len(board)):
+            for col in [0, len(board[0]) - 1]:
+                if board[row][col] == "O":
+                    visit(row, col)
 
 
         def capture(row, col):
@@ -34,16 +42,14 @@ class Solution:
                 new_row = dr + row
                 new_col = dc + col
 
-                if board[new_row][new_col] == "O" :
+                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O" :
                     capture(new_row, new_col)
 
 
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if board[row][col] == "O" and check(row, col):
+                if board[row][col] == "O" and (row, col) not in visited:
                     capture(row, col)
-
-                visited = set()
 
 
 
