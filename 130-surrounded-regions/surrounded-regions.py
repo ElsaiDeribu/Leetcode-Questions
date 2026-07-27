f8886b8c026041c9e9e91f58ab5027f05edbadc0
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-07-27 13:03:18 -0400

    Time: 3 ms (83.48%) | Memory: 22.4 MB (59.66%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -10,46 +10,37 @@ class Solution:
             return 0 <= row < len(board) and 0 <= col < len(board[0])
 
         
-        def visit(row, col):
+        def mark(row, col):
             
-            visited.add((row, col))
+            board[row][col] = "T"
             
             for dr, dc in dirs:
                 new_row = dr + row
                 new_col = dc + col
 
-                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O" and (new_row, new_col) not in visited:
-                    visit(new_row, new_col)
+                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O":
+                    mark(new_row, new_col)
                     
 
         for row in [0, len(board) - 1]:
             for col in range(len(board[0])):
                 if board[row][col] == "O":
-                    visit(row, col)
+                    mark(row, col)
 
 
         for row in range(len(board)):
             for col in [0, len(board[0]) - 1]:
                 if board[row][col] == "O":
-                    visit(row, col)
+                    mark(row, col)
 
-
-        def capture(row, col):
-
-            board[row][col] = "X"
-
-            for dr, dc in dirs:
-                new_row = dr + row
-                new_col = dc + col
-
-                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O" :
-                    capture(new_row, new_col)
 
 
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if board[row][col] == "O" and (row, col) not in visited:
-                    capture(row, col)
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+                elif board[row][col] == "T":
+                    board[row][col] = "O"
 
 
 
