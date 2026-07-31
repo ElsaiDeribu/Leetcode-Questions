7c66254f4e7ecf38f5265b75a907c3e3ec15e46e
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-07-31 15:43:53 -0400

    Time: 441 ms (5.05%) | Memory: 19.7 MB (32.44%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,21 +1,27 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+
+        ans = set()
+        perm = []
+
+        def dfs(left_over):
+            if left_over < 0:
+                return 
+
+            if left_over == 0:
+                ans.add(tuple(sorted(perm[:])))
+                return
+
+            for _, val  in enumerate(candidates):
+                perm.append(val)
+                dfs(left_over - val)
+                perm.pop()
+
+        dfs(target)
+
+        return list(ans)
+
+
+
+
         
-        ans = []
-
-        def dfs(i, total, path):
-            if total == target:
-                ans.append(path[:])
-                return
-            if total > target or i == len(candidates):
-                return
-
-            # Choose the current candidate
-            dfs(i, total + candidates[i], path + [candidates[i]])
-            # Skip to the next candidate
-            dfs(i + 1, total, path)
-
-        dfs(0, 0, [])
-        return ans
-
-
