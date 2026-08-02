a20cddfca3e52446ba4934cdadcf33749364f6b7
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-02 19:14:35 -0400

    Time: 3 ms (61.21%) | Memory: 20.4 MB (21.41%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
         
-        @cache
-        def dfs(idx, last_action):
-            if idx == len(prices): return 0
+        profit = 0
+        curr = prices[0]
 
-            if last_action == "buy":
-                res1 = dfs(idx + 1, last_action)
-                res2 = dfs(idx + 1, "sell") + prices[idx]
+        for price in prices:
 
-                return max(res1, res2)
+            if price > curr:
+                profit += price - curr
+                curr = price
 
             else:
-                res1 = dfs(idx + 1, last_action)
-                res2 = dfs(idx + 1, "buy") - prices[idx]
-
-                return max(res1, res2)
+                curr = min(curr, price)
 
 
-        return dfs(0, "")
+        return profit
