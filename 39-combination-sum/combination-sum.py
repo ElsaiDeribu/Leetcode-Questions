167aaf41d110110474754fdc8dca0c1abaffa834
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-12 12:36:24 -0400

    Time: 14 ms (23.08%) | Memory: 19.8 MB (8.96%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,6 +1,6 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]: 
+        
         ans = []
         perm = []
 
@@ -13,11 +13,9 @@ class Solution:
                 ans.append(perm[:])
                 return
 
-
             perm.append(candidates[idx])
             dfs(idx, left_over - candidates[idx])
             perm.pop()
-
 
             dfs(idx + 1, left_over)
 
@@ -25,6 +23,33 @@ class Solution:
         dfs(0, target)
 
         return ans
+      
+        
+        
+        # ans = []
+        # perm = []
+
+        # def dfs(idx, left_over):
+
+        #     if left_over < 0 or idx == len(candidates):
+        #         return 
+
+        #     if left_over == 0:
+        #         ans.append(perm[:])
+        #         return
+
+
+        #     perm.append(candidates[idx])
+        #     dfs(idx, left_over - candidates[idx])
+        #     perm.pop()
+
+
+        #     dfs(idx + 1, left_over)
+
+
+        # dfs(0, target)
+
+        # return ans
 
 
 
