2c0ee67965e170c02b4b035fb9050cfd1f9e48ae
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-13 13:14:06 -0400

    Time: 12 ms (29.22%) | Memory: 19.6 MB (64.69%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]: 
         
+
         ans = []
         perm = []
 
         def dfs(idx, left_over):
 
-            if left_over < 0 or idx == len(candidates):
-                return 
-
             if left_over == 0:
                 ans.append(perm[:])
                 return
+
+            if left_over < 0 or idx == len(candidates):
+                return 
 
             perm.append(candidates[idx])
             dfs(idx, left_over - candidates[idx])
