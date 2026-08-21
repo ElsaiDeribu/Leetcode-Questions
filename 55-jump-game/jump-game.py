c4282531b30d7181bd43efb134f1321b8a41324a
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-21 16:23:38 -0400

    Time: 25 ms (30.23%) | Memory: 20.6 MB (15.75%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,11 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
+        # TC: O(n)
+        # SC: O(1)
+
         tank = nums[0]
 
         for i in range(len(nums) - 1):
             tank = max(nums[i], tank)
             if tank == 0: return False
             tank -= 1
+
 
         return True
